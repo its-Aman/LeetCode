655507ca71ea9061deda3666a19aa80525fc21db
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-16 17:17:49 +0530

    Time: 114 ms, Memory: 44.3 MB - LeetHub

---
+++ powx-n/powx-n.ts
@@ -0,0 +1,32 @@
+/*
+function myPow(x: number, n: number): number {
+  let ans = 1;
+  let isNeg = n < 0;
+  n = Math.abs(n);
+  
+  while(n){
+    ans *= x;
+    n--;    
+  }
+  
+  if(isNeg)
+    ans = 1/ans;
+  
+  return ans;
+};
+*/
+
+function myPow(x: number, n: number): number {
+  if(n == 0)
+    return 1;
+  
+  if(n < 0)
+    return myPow(1/x, -n);
+  
+  const xPow2 = myPow(x, Math.trunc(n / 2));
+  
+  if(n % 2 == 0)
+    return xPow2 * xPow2;
+  
+  return x * xPow2 * xPow2;
+};
