9ce0127fcc3cae4eab9dd8369701a6aae4d459a0
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-16 13:43:39 +0530

    Time: 210 ms (55.14%), Space: 45.1 MB (45.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.ts
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.ts
@@ -1,23 +1,22 @@
-function lengthOfLIS(nums: number[]): number {  
+/*
+https://www.youtube.com/watch?v=odrfUCS9sQk
+*/
+
+function lengthOfLIS(nums: number[]): number {
   let max = 0,
       N = nums.length,
-      dp = new Array(N - 1).fill(1);
+      dp = new Array(N).fill(1);
   
-  if(N == 1)
-    return 1;
+  if(N <= 1)
+    return N;
   
   for(let i = 1; i < N; i++){
-    let currMax = 1;
     for(let j = 0; j < i; j++){
-      // console.log(nums[j], nums[i], currMax, dp[j]);
       if(nums[j] < nums[i])
-        currMax = Math.max(currMax, 1 + dp[j])
+        dp[i] = Math.max(dp[i], 1 + dp[j])
     }
-    dp[i] = currMax;
     max = Math.max(max, dp[i]);
   }
-  
-//   console.log(dp);
-  
+
   return max;
 };
