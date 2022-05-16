4f5edfc870baed04fe7eb5f503b0bb1877db9dc3
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-16 17:44:09 +0530

    Time: 107 ms, Memory: 45.1 MB - LeetHub

---
+++ sqrtx/sqrtx.ts
@@ -0,0 +1,21 @@
+function mySqrt(x: number): number {  
+  let l = 1,
+      r = x;
+  
+  if(x < 2)
+    return x;
+  
+  
+  while(l < r){
+    const mid = l + Math.floor((r - l) / 2),
+          num = Math.floor(x / mid);
+    if(num == mid)
+      return mid;
+    else if(num < mid)
+      r = mid;
+    else 
+      l = mid + 1;      
+  }
+  
+  return l - 1;
+};
