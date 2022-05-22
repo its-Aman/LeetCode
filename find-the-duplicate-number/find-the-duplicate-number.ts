dd2c907ebcf58d04520ac5e89b159e0638fdfa3b
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-22 06:38:07 +0530

    Time: 2541 ms, Memory: 52 MB - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.ts
+++ find-the-duplicate-number/find-the-duplicate-number.ts
@@ -1,4 +1,6 @@
-function findDuplicate(nums: number[]): number {
+// Solution: Approach 5: Binary Search
+
+function findDuplicate_BinarySearch(nums: number[]): number {
   const N = nums.length
   let l = 0,
       h = N - 1,
@@ -21,3 +23,36 @@ function findDuplicate(nums: number[]): number {
   }
   return dup
 };
+
+function findDuplicate(nums: number[]): number {
+  const N = nums.length,
+        maxBit = calcMaxBit(nums);
+  let dup = 0;
+  
+  for(let bit = 0; bit < maxBit; bit++){
+    let mask = (1 << bit),
+        base_count = 0,
+        num_count = 0;
+    
+    for(let i = 0; i < N; i++){
+      if((i & mask) > 0)
+        base_count++;
+      
+      if((nums[i] & mask) > 0)
+        num_count++;
+    }
+    if(num_count > base_count)
+      dup |= mask;
+  }
+  return dup;
+}
+
+function calcMaxBit(nums: number[]){ 
+  let num = Math.max(...nums),
+      bits = 0;
+  while(num > 0){
+    num /= 2;
+    bits++;
+  }
+  return bits;
+}
