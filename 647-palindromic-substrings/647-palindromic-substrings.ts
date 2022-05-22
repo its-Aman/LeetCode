a93435d000b7cf1cd80808b5fa04fe71ab7f8a63
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-22 07:41:48 +0530

    Time: 82 ms (86.15%), Space: 43.8 MB (59.23%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.ts
+++ 647-palindromic-substrings/647-palindromic-substrings.ts
@@ -1,6 +1,6 @@
 // https://www.youtube.com/watch?v=XmSOWnL6T_I
 
-function countSubstrings(s: string): number {
+function countSubstrings_DP_Method(s: string): number {
   let N = s.length,
       dp = Array.from(new Array(N), () => new Array(N).fill(false)),
       count = 0;
@@ -21,3 +21,23 @@ function countSubstrings(s: string): number {
   }
   return count;
 };
+
+function countSubstrings(s: string): number {
+  let res = 0;
+  
+  for(let i = 0; i < s.length; i++)
+    res += (countPalindrome(s, i, i) + countPalindrome(s, i, i + 1));
+
+  return res;
+}
+
+function countPalindrome(s: string, l: number, r: number): number{
+  let count = 0;
+  
+  while((l >= 0 && r < s.length) && s.charAt(l) == s.charAt(r)){
+    count++;
+    l--;
+    r++;
+  }
+  return count;
+}
