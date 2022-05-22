8a9ab5c0286c130b9b0ea4fbbcdfd0ac581abb5f
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-22 20:29:02 +0530

    Time: 112 ms (34.43%), Space: 45.4 MB (30.33%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.ts
+++ 496-next-greater-element-i/496-next-greater-element-i.ts
@@ -7,15 +7,15 @@ function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
   let i = N2 - 1;
   
   while(i >= 0){
-    if(stack.length == 0){
-      map.set(nums2[i], -1);
-    } else if(stack[stack.length - 1] > nums2[i]){
-      map.set(nums2[i], stack[stack.length - 1]);
-    } else {
+    // if(stack.length == 0){
+    //   map.set(nums2[i], -1);
+    // } else if(stack[stack.length - 1] > nums2[i]){
+    //   map.set(nums2[i], stack[stack.length - 1]);
+    // } else {
       while(stack.length != 0 && (stack[stack.length - 1] <= nums2[i]))
         stack.pop();
       map.set(nums2[i], stack.length > 0 ? stack[stack.length - 1] : -1);
-    }
+    // }
     stack.push(nums2[i]);
     i--;
   }
