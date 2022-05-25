3451be78897994054b29cb27d714b522743307b0
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-25 08:09:19 +0530

    Time: 388 ms (6.25%), Space: 74.6 MB (43.75%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.ts
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.ts
@@ -2,9 +2,10 @@
 
 function maxEnvelopes(envelopes: number[][]): number {
   envelopes.sort((a,b) => (a[0] == b[0] ? (b[1]-a[1]) : a[0] - b[0]));
-  let dp = [],
-      N = envelopes.length;
-  // console.log({envelopes});
+  
+  const dp = [],
+        N = envelopes.length;
+  
   for(let i = 0; i < N; i++){
     let h = envelopes[i][1],
         l = 0,
@@ -12,14 +13,12 @@ function maxEnvelopes(envelopes: number[][]): number {
     
     while(l < r){
       const m = (l + r) >> 1;
-      // console.log({i, l, r, m, h, dpm: dp[m]})
       if(dp[m] < h)
         l = m + 1;
       else 
         r = m
     }
     dp[l] = h;
-    // console.log({dp})
   }
   return dp.length;
 };
