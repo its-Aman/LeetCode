a62add16226af3d20f964f1e62e0fcef8840a011
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-28 22:26:51 +0530

    Time: 80 ms (79.84%), Space: 45.4 MB (53.68%) - LeetHub

--- 268-missing-number/268-missing-number.ts
+++ 268-missing-number/268-missing-number.ts
@@ -1,6 +1,17 @@
+/*
 function missingNumber(nums: number[]): number {
   const N = nums.length,
         sigma = (N * (N + 1)) / 2,
         sum = nums.reduce((acc, curr) => acc + curr);
   return sigma - sum;
 };
+*/
+
+function missingNumber(nums: number[]): number {
+  let xor = 0, i = 0;
+  
+  for(let num of nums)
+    xor ^= (num ^ i++);
+  
+  return xor ^ i;
+}
