319ad34523aa232a3dc7c18b6fc7f53995570d78
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-29 13:52:14 +0530

    Time: 153 ms (60.00%), Space: 46.2 MB (80.00%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.ts
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.ts
@@ -1,4 +1,22 @@
 function maxProduct(words: string[]): number {
+  const N = words.length,
+        mask = new Array(N).fill(0);
+  let max = 0;
+
+  for(let i = 0; i < N; i++)
+    for(let c of words[i])
+      mask[i] |= (1 << (c.charCodeAt(0) - 'a'.charCodeAt(0)));
+
+  for(let i = 0; i < N; i++)
+    for(let j = i + 1; j < N; j++)
+      if((mask[i] & mask[j]) == 0)
+        max = Math.max(max, words[i].length * words[j].length);
+  
+  return max;
+  
+}
+/*
+function maxProduct(words: string[]): number {
   const N = words.length;
   let max = 0;
   
@@ -18,3 +36,5 @@ function checkForCommonChar(s1: string, s2: string): boolean {
   }
   return false;
 }
+
+*/
