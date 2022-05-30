4cb4f4f3fe69ebc82ee1ca75e1d9ce08139fd8ab
Aman Kumar <kumaraman7056@hotmail.com>
2022-05-30 18:13:13 +0530

    Time: 92 ms (70.33%), Space: 45 MB (51.65%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.ts
@@ -0,0 +1,31 @@
+// Worst question I've encountered on LC. 
+// Don't know why they've put it in daily challange. 
+// Waste my whole day !!!
+
+function divide(A: number, B: number): number {
+  if (A === -2147483648 && B === -1) 
+    return 2147483647
+  
+  let ans = 0, sign = 1
+  if (A < 0){
+    A = -A;
+    sign = -sign;  
+  } 
+
+  if (B < 0){
+    B = -B;
+    sign = -sign
+  } 
+  
+  if (A === B) 
+    return sign
+  
+  for(let i = 0, val = B; A >= B; i = 0, val = B) {
+    while (val > 0 && val <= A) 
+      val = (B << (++i));
+    
+    A -= B << i - 1, ans += 1 << i - 1
+  }
+  
+  return sign < 0 ? -ans : ans
+};
