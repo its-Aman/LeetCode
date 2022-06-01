d728ad404494fd485bc12dc72f8d06c3dab59338
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-01 06:27:12 +0530

    Time: 173 ms, Memory: 53.2 MB - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.ts
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.ts
@@ -12,6 +12,7 @@
  * }
  */
 
+/*
 // DFS Recursive approach
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
@@ -34,4 +35,46 @@ function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: Tree
     
     return (left + right + mid > 0);
   }
+}
+
+*/
+
+
+// Iterative using parent pointer.
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+  const stack = [], // stack for tree traversal
+        parent = new Map<TreeNode, TreeNode>(), // map for parent pointers
+        ancestors = new Set<TreeNode>(); // ancestors set for node p
+  
+  parent.set(root, null); // currentNode, parentNode
+  stack.push(root); // a kind of dfs call
+  
+//   Lets track all the parents until we find p or q
+  while(!parent.has(p) || !parent.has(q)){
+    const node = stack.pop();
+    
+//     keep saving the parents of current nodes.
+    if(node.right){
+      parent.set(node.right, node);
+      stack.push(node.right);
+    }
+
+    if(node.left){
+      parent.set(node.left, node);
+      stack.push(node.left);
+    }
+  }
+  
+//   here is the main part, we'll now be processing ancestors of p and keeping them safe in a set.
+  while(p){
+    ancestors.add(p);
+    p = parent.get(p);
+  }
+  
+//   now we just iterate over ancestors set to find the parent of both p and q
+  while(!ancestors.has(q))
+    q = parent.get(q);
+  
+  return q;
 }
