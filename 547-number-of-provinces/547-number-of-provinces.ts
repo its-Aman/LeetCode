55e5f007eb78cb78d278e531433b201e50cc884c
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-02 06:35:07 +0530

    Time: 149 ms (19.08%), Space: 44.9 MB (87.50%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.ts
+++ 547-number-of-provinces/547-number-of-provinces.ts
@@ -1,4 +1,72 @@
+// https://www.youtube.com/watch?v=wUgWX0nc4NY
+
 function findCircleNum(isConnected: number[][]): number {
+  // return tarjanAlgo(isConnected);
+  
+  const N = isConnected.length;
+  const uf = new UnionFind(N);
+  
+  for(let i = 0; i < N; i++)
+    for(let j = i + 1; j < N; j++)
+      if(isConnected[i][j])
+        uf.union(i, j);
+
+  return uf.count();
+}
+
+// https://leetcode.com/problems/number-of-provinces/discuss/101336/Java-solution-Union-Find
+
+class UnionFind {
+  private _count: number;
+  private parent: number[];
+  private rank: number[];
+
+  public constructor(size: number) {
+    this._count = size;
+    this.parent = new Array(size);
+    this.rank = new Array(size).fill(1);
+    
+    for(let i = 0; i < size; i++)
+      this.parent[i] = i;
+    
+  }
+
+  public find(p: number): number {
+    while(p != this.parent[p]){
+      this.parent[p] = this.parent[this.parent[p]];
+      p = this.parent[p];
+    }
+    
+    return p;
+  }
+
+  public union(p: number, q: number): void{
+    const rootP = this.find(p),
+          rootQ = this.find(q);
+    
+    if(rootP == rootQ)
+      return;
+    
+    if(this.rank[rootQ] > this.rank[rootP]){
+      this.parent[rootP] = rootQ;
+    } else {
+      this.parent[rootQ] = rootP;
+      
+      if(this.rank[rootQ] == this.rank[rootP])
+        this.rank[rootP]++;
+    }
+
+    this._count--;
+  }
+  
+  public count(): number {
+    return this._count;
+  }
+}
+
+// tarjan's low link algorithm
+
+function tarjanAlgo(isConnected: number[][]): number {
   const N = isConnected.length;
   const g = buildGraph(isConnected);
   let id = 0,
