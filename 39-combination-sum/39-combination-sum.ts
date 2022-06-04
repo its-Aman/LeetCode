41b13746580f1181226cb7e4beeb3206bf435205
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-04 21:00:48 +0530

    Time: 104 ms (67.63%), Space: 48.5 MB (39.93%) - LeetHub

--- 39-combination-sum/39-combination-sum.ts
+++ 39-combination-sum/39-combination-sum.ts
@@ -2,6 +2,8 @@ function combinationSum(candidates: number[], target: number): number[][] {
   const N = candidates.length,
         ans = [];
   
+  candidates.sort((a, b) => a - b);
+  
   for(let i = 0; i < N; i++){
     backtrack(i, candidates[i], [candidates[i]])
   }
@@ -18,6 +20,9 @@ function combinationSum(candidates: number[], target: number): number[][] {
       return;
     
     for(let j = i; j < N; j++){
+      if(currSum + candidates[j] > target)
+        break;
+
       group.push(candidates[j])
       backtrack(j, currSum + candidates[j], group.slice(0));
       group.pop();
