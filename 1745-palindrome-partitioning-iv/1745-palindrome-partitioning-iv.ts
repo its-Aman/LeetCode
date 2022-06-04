d3f17c48a9edd57b1a64b504697e37a6be5f3981
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-04 16:56:42 +0530

    Time: 700 ms (100.00%), Space: 106.1 MB (100.00%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.ts
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.ts
@@ -1,4 +1,38 @@
-function checkPartitioning(s: string): boolean {
+function checkPartitioning(S: string): boolean {
+  const N = S.length,
+        dp = Array.from(Array(N), () => Array(N).fill('_'.repeat(4)));
+  
+  for(let i = N - 1; i >= 0; i--){
+    for(let j = i; j < N; j++){
+      const len = j - i + 1;
+      if(len == 1){
+        dp[i][j] = true;
+      } else if(len == 2){
+        dp[i][j] = S.charAt(i) == S.charAt(j);
+      } else {
+        dp[i][j] = (S.charAt(i) == S.charAt(j)) && dp[i + 1][j - 1];        
+      }
+      // console.log({i, j, len}, S.charAt(i), S.charAt(j), S.substring(j, i), dp[i][j])
+    }
+  }
+  
+  // console.log(dp);
+  
+  for(let p = 0;  p < N; p++){
+    for(let s = p + 2; s < N; s++){
+      const left = dp[0][p],
+            mid = dp[p + 1][s - 1],
+            right = dp[s][N - 1];
+      // console.log({l: S.substring(0, p+1), m: S.substring(p+1, s), r: S.substring(s, N)}, {p, s})
+      if(left && mid && right)
+        return true;
+    }
+  }
+  
+  return false;
+}
+
+function checkPartitioning_BRUTE_FORCE(s: string): boolean {
   const N = s.length;
   
   for(let i = 2; i < N; i++){
