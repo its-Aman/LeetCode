96e8a294793355e4348f04a560dc5a00af381c70
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-05 06:08:47 +0530

    Time: 77 ms (91.00%), Space: 45.7 MB (46.00%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.ts
+++ 90-subsets-ii/90-subsets-ii.ts
@@ -1,23 +1,27 @@
 function subsetsWithDup(nums: number[]): number[][] {
   const N = nums.length,
         result = [];
-  
   nums.sort((a, b) => a - b);
   
   backtrack(result, [], 0);
   
   return result;
   
-  function backtrack(result: number[][], curr: number[], start: number){
-    result.push(curr);
+  function backtrack(result: number[][], curr: number[], i: number){
+    result.push(curr.slice(0));
+ 
+    const set = new Set<number>();
     
-    for(let i = start; i < N; i++){
-      if(i > start && nums[i] == nums[i - 1])
+    for(let j = i; j < N; j++){
+      if(set.has(nums[j]))
         continue;
       
-      curr.push(nums[i]);
-      backtrack(result, curr.slice(0), i + 1);
+      set.add(nums[j]);
+
+      curr.push(nums[j]);
+      backtrack(result, curr, j + 1);
       curr.pop();
     }
   }
+  
 };
