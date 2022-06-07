347b929c29defe718f7bcf6f2a3bd497f435a48e
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-07 06:24:01 +0530

    Time: 314 ms, Memory: 74 MB - LeetHub

--- implement-trie-prefix-tree/implement-trie-prefix-tree.ts
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.ts
@@ -1,58 +1,82 @@
-class TrieNode<T> {
-  public children: Map<T, TrieNode<T>>;
+class TrieNode {
+  public children: Map<string, TrieNode>;
   public isEnd: boolean;
+  public word: string;
 
-  constructor() {
-    this.children = new Map<T, TrieNode<T>>();
-    this.isEnd = false;
+  constructor(isEnd = false){
+    this.children = new Map<string, TrieNode>();
+    this.isEnd = isEnd;
+    this.word = '';
   }
 }
 
 class Trie {
-  private root: TrieNode<string>;
-
-  constructor() {
-    this.root = new TrieNode<string>();
+  root: TrieNode;
+  
+  constructor(){
+    this.root = new TrieNode();    
   }
-
-  insert(word: string): void {
+  
+  public insert(key: string) {
     let curr = this.root;
-    for (let c of word) {
-      if (!curr.children.has(c)) {
-        curr.children.set(c, new TrieNode<string>());
-      }
+    
+    for(let c of key){
+      if(!curr.children.has(c))
+        curr.children.set(c, new TrieNode());
+      
       curr = curr.children.get(c);
     }
     curr.isEnd = true;
+    curr.word = key;
   }
 
-  search(word: string): boolean {
+  public getWord(key: string): string {
     let curr = this.root;
-    for (let c of word) {
-      if (!curr.children.has(c)) 
-        return false;
+    
+    for(let c of key){
+      if(!curr.children.has(c) || curr.word)
+        break;
+      
+      curr = curr.children.get(c);
+    }
+    return curr.word || key;
+  }
 
+  public search(key: string): boolean {
+    let curr = this.root;
+    
+    for(let c of key){
+      if(!curr.children.has(c))
+        return false;
+      
       curr = curr.children.get(c);
     }
     return curr.isEnd;
   }
 
-  startsWith(prefix: string): boolean {
+  public startsWith(key: string): boolean {
     let curr = this.root;
-    for (let c of prefix) {
-      if (!curr.children.has(c))
+    
+    for(let c of key){
+      if(!curr.children.has(c))
         return false;
-
+      
       curr = curr.children.get(c);
     }
     return true;
   }
 }
 
-/**
- * Your Trie object will be instantiated and called as such:
- * var obj = new Trie()
- * obj.insert(word)
- * var param_2 = obj.search(word)
- * var param_3 = obj.startsWith(prefix)
- */
+function replaceWords(dictionary: string[], sentence: string): string {
+  const trie = new Trie(),
+        keys = sentence.split(' ');
+  let result = '';
+  
+  for(let d of dictionary)
+    trie.insert(d);
+  
+  for(let key of keys)
+    result = result.concat(trie.getWord(key), ' ');
+  
+  return result.trim();
+};
