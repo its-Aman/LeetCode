84114661d0197758a9a5d1afa75ac5a85aff06ad
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-12 20:04:13 +0530

    Time: 143 ms (59.14%), Space: 45.5 MB (62.90%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.ts
+++ 10-regular-expression-matching/10-regular-expression-matching.ts
@@ -1,4 +1,31 @@
 function isMatch(s: string, p: string): boolean {
+  // return isMatch_Recursion(s, p);
+  return isMatch_DP(s, p);
+}
+
+function isMatch_DP(s: string, p: string): boolean {
+  const dp = Array.from(new Array<boolean>(s.length + 1), () => new Array<boolean>(p.length + 1).fill(false));
+  
+  dp[0][0] = true;
+  
+  for(let i = 1; i <= p.length; i++)
+    if(p.at(i - 1) == '*')
+      dp[0][i] = dp[0][i - 2];
+  
+  for(let i = 1; i <= s.length; ++i)
+    for(let j = 1; j <= p.length; ++j)
+      if(p.at(j - 1) == s.at(i - 1) || p.at(j - 1) == '.')
+        dp[i][j] = dp[i - 1][j - 1];
+      else if(p.at(j - 1) == '*')
+        if(p.at(j - 2) == s.at(i - 1) || p.at(j - 2) == '.')
+          dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+        else
+          dp[i][j] = dp[i][j - 2];
+  
+  return dp[s.length][p.length];
+}
+
+function isMatch_Recursion(s: string, p: string): boolean {
   if(!p.length)
     return s.length == 0;
   
