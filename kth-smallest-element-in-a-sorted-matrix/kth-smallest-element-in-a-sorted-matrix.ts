199f2e3e38b4cc1510225aac7fd6affdc386e74a
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-15 10:29:02 +0530

    Time: 104 ms, Memory: 45.7 MB - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.ts
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.ts
@@ -7,17 +7,30 @@ function kthSmallest(matrix: number[][], k: number): number {
   
   while(low < hi) {
     const mid = low + ((hi - low) >>> 1);
-    let j = N - 1,
-        count = 0;
-    for(let i = 0; i < N; i++) {
-      while(j >= 0 && matrix[i][j] > mid)
-        j--;
-      count += j + 1;
-    }
+     let count = 0;
+    
+    for(let i = 0; i < N; i++) 
+      count += bisect_right(i, mid);
+    
     if(count < k)
       low = mid + 1;
     else
       hi = mid;
   }
   return low;
+
+  function bisect_right(i: number, target: number): number {
+    let low = 0,
+        hi = N;
+    
+    while(low < hi) {
+      const mid = low + ((hi - low) >>> 1);
+      if(matrix[i][mid] <= target)
+        low = mid + 1;
+      else
+        hi = mid;
+    }
+    
+    return low;
+  }
 };
