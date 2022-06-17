d04c0d1bd165d6fd0bfd96cd332425f291632e3f
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-17 07:59:06 +0530

    Time: 102 ms (46.14%), Space: 42.9 MB (50.50%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.ts
+++ 509-fibonacci-number/509-fibonacci-number.ts
@@ -3,7 +3,7 @@ function fib(n: number, map = new Map<number, number>()): number {
     return n;
   
   if(!map.has(n))
-    map.set(n, fib(n-1) + fib(n-2));
+    map.set(n, fib(n-1, map) + fib(n-2, map));
   
   return map.get(n);
 };
