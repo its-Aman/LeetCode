2c62426f7f204932bc0d6b5e530a1d05ae937c2f
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-26 07:09:07 +0530

    Time: 177 ms (100.00%), Space: 51.4 MB (100.00%) - LeetHub

---
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.ts
@@ -0,0 +1,22 @@
+function maximumXOR(nums: number[]): number {
+  let bits = new Array(32).fill(0),
+      mask = 0;
+  
+  for(let num of nums) {
+    for(let i = 0; i < 32; i++) {
+      if(num & (1 << i))
+        bits[i]++;
+      
+      // console.log({num, i, sft: 1 << i, and: num & (1 << i), b: bits[i]})
+    }
+    // console.log(bits)
+  }
+  
+  for(let i = 0; i < 32; i++) {
+    if(bits[i])
+      mask |= (1 << i);
+    // console.log({mask, b: bits[i], sft: 1 << i})
+  }
+
+  return mask;
+};
