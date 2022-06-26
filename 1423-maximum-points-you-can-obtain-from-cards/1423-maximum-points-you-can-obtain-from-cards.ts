f15431b4a493ee6e331f4f35f550ee78fac47f0c
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-26 20:09:18 +0530

    Time: 136 ms (25.00%), Space: 60.5 MB (6.25%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.ts
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.ts
@@ -4,18 +4,11 @@ function maxScore(cardPoints: number[], K: number): number {
   
   let ans = 0;
   
-  prefixSum[0] = 0;
-  
   for(let [i, ptx] of cardPoints.entries())
     prefixSum[i + 1] = prefixSum[i] + ptx;
   
-  // console.log(prefixSum);
-  
-  for(let i = 0; i <= K; i++) {
-    const j = N - K + i;
-    // console.log(i, j, ans, prefixSum[i], prefixSum[N], prefixSum[j])
-    ans = Math.max(ans, prefixSum[i] + (prefixSum[N] - prefixSum[j]))
-  }
+  for(let i = 0; i <= K; i++)
+    ans = Math.max(ans, prefixSum[i] + (prefixSum[N] - prefixSum[N - K + i]))
   
   return ans;
 };
