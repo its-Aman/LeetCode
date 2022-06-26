fbf951be6a5df110d842fdd841b9df3d8640bf7c
Aman Kumar <kumaraman7056@hotmail.com>
2022-06-26 10:50:46 +0530

    Time: 97 ms (100.00%), Space: 52.8 MB (100.00%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.ts
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.ts
@@ -1,23 +1,29 @@
-function maximumsSplicedArray_mine(nums1: number[], nums2: number[]): number {
-  const N = nums1.length,
-        temp1 = new Array<number>(N + 1).fill(0),
-        temp2 = new Array<number>(N + 1).fill(0);
-  let sum1 = 0,
-      sum2 = 0;
+function maximumsSplicedArray(nums1: number[], nums2: number[]): number {
+  let N = nums1.length,
+      temp1 = 0,
+      temp2 = 0,
+      sum1 = 0,
+      sum2 = 0,
+      res1 = 0,
+      res2 = 0;
   
   for(let i = 0; i < N; i++) {
     sum1 += nums1[i];
     sum2 += nums2[i];
 
     const diff = nums1[i] - nums2[i];
-    temp1[i + 1] = Math.max(0, -1 * diff + temp1[i]);
-    temp2[i + 1] = Math.max(0, diff + temp2[i]);
+    
+    temp1 = Math.max(0, -1 * diff + temp1);
+    res1 = Math.max(res1, temp1);
+    
+    temp2 = Math.max(0, diff + temp2);
+    res2 = Math.max(res2, temp2);
   }
   
-  return Math.max(sum1 + Math.max(...temp1), sum2 + Math.max(...temp2));
+  return Math.max(sum1 + res1, sum2 + res2);
 };
 
-function maximumsSplicedArray(nums1: number[], nums2: number[]): number {
+function maximumsSplicedArray_rec(nums1: number[], nums2: number[]): number {
   
   return Math.max(kadane(nums1, nums2), kadane(nums2, nums1));
   
