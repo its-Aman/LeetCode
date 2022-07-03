30b1f0c17117c9881e2f906e97e40c7d3cf62a1f
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-03 07:05:50 +0530

    Time: 78 ms (72.73%), Space: 42.8 MB (63.64%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.ts
@@ -0,0 +1,56 @@
+function wiggleMaxLength(nums: number[]): number {
+  const N = nums.length,
+        dp = new Array(N).fill(1);
+  let upDP = 1,
+      downDP = 1; 
+  
+  for(let i = 1; i < N; i++) {
+    if(nums[i] > nums[i - 1]) {
+      upDP = 1 + downDP;
+    } else if(nums[i] < nums[i - 1]) {
+      downDP = 1 + upDP;
+    }
+  }
+
+  return Math.max(upDP, downDP);
+}
+
+function wiggleMaxLength_N(nums: number[]): number {
+  const N = nums.length,
+        dp = new Array(N).fill(1),
+        upDP = new Array(N).fill(1),
+        downDP = new Array(N).fill(1); 
+  
+  for(let i = 1; i < N; i++) {
+    if(nums[i] > nums[i - 1]) {
+      upDP[i] = 1 + downDP[i - 1];
+      downDP[i] = downDP[i - 1];
+    } else if(nums[i] < nums[i - 1]) {
+      downDP[i] = 1 + upDP[i - 1];
+      upDP[i] = upDP[i - 1];
+    } else {
+      upDP[i] = upDP[i - 1];
+      downDP[i] = downDP[i - 1];
+    }
+  }
+
+  return Math.max(upDP[N - 1], downDP[N - 1]);
+}
+
+function wiggleMaxLength_N2(nums: number[]): number {
+  const N = nums.length,
+        dp = new Array(N).fill(1),
+        upDP = new Array(N).fill(0),
+        downDP = new Array(N).fill(0); 
+  
+  for(let i = 1; i < N; i++) {
+    for(let j = 0; j < i; j++) {
+      if(nums[i] > nums[j])
+        upDP[i] = Math.max(upDP[i], 1 + downDP[j]);
+      else
+        downDP[i] = Math.max(downDP[i], 1 + upDP[j])
+    }
+  }
+
+  return 1 + Math.max(upDP[N - 1], downDP[N - 1]);
+};
