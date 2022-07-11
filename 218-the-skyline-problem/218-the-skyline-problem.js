6fa67847b2db8f06433815a0b99673bc1d6e9944
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-11 12:19:42 +0530

    Time: 3136 ms (5.17%), Space: 72.8 MB (5.17%) - LeetHub

---
+++ 218-the-skyline-problem/218-the-skyline-problem.js
@@ -0,0 +1,64 @@
+/**
+ * @param {number[][]} buildings
+ * @return {number[][]}
+ */
+
+function MultiSet() {
+    let tm = {}; // treemap: works for key >= 0
+    return { insert, eraseOne, erase, contains, first, last, show }
+    function insert(x) {
+        tm[x] ? tm[x]++ : tm[x] = 1;
+    }
+    function eraseOne(x) {
+        let occ = tm[x];
+        occ > 1 ? tm[x]-- : delete tm[x];
+    }
+    function erase(x) {
+        delete tm[x];
+    }
+    function contains(x) {
+        return tm[x] ? 1 : 0;
+    }
+    function first() {
+        let a = Object.keys(tm);
+        return a[0] - '0';
+    }
+    function last() {
+        let a = Object.keys(tm);
+        return a[a.length - 1] - '0';
+    }
+    function show() {
+        console.log(tm);
+    };
+}
+
+var getSkyline = function(buildings) {
+  const N = buildings.length,
+        buildingPoints = new Array(),
+        ms = new MultiSet(),
+        ans = new Array();
+  
+  for(let [x, y, h] of buildings)
+    buildingPoints.push([x, -h], [y, h]);
+
+  buildingPoints.sort((a, b) => (a[0] != b[0]) ? a[0] - b[0] : a[1] - b[1]);
+
+  ms.insert(0);
+  let pre = 0;
+  
+  for(let [x, h] of buildingPoints) {
+    if(h < 0)
+      ms.insert(-h);
+    else
+      ms.eraseOne(h);
+    
+    let curr = ms.last();
+    
+    if(curr != pre) {
+      ans.push([x, curr]);
+      pre = curr;
+    }
+  }
+  
+  return ans;
+};
