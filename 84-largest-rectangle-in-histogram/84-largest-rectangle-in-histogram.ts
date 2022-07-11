e8e100e8d74a6e20654826d6cd06e38f63fe9073
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-11 13:02:25 +0530

    Time: 211 ms (37.14%), Space: 79.2 MB (11.43%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.ts
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.ts
@@ -1,27 +1,23 @@
-// https://www.geeksforgeeks.org/largest-rectangle-under-histogram/
-
 function largestRectangleArea(heights: number[]): number {
+
   const N = heights.length,
-        stack = [];
-  let maxArea = 0,
-      i = 0;
+        stack = new Array();
+  let maxArea = 0;
+  
+  for(let i = 0; i < N; i++) {
+    let start = i;
     
-  while(i < N){    
-    if(stack.length == 0 || (heights[stack[stack.length - 1]]) <= heights[i]){
-      stack.push(i);
-      i++;
-    } else {
-      const top = stack.pop(),
-            area = heights[top] * (stack.length > 0 ? (i - stack[stack.length - 1] - 1) : i);
-      maxArea = Math.max(maxArea, area);
+    while(stack.length && stack[stack.length - 1][1] > heights[i]) {
+      const [idx, ht] = stack.pop();
+      maxArea = Math.max(maxArea, ht * (i - idx));
+      start = idx;
     }
+    
+    stack.push([start, heights[i]]);
   }
   
-  while(stack.length > 0){
-    const top = stack.pop(),
-          area = heights[top] * (stack.length > 0 ? (i - stack[stack.length - 1] - 1) : i);
-    maxArea = Math.max(maxArea, area);
-  }
+  for(let [idx, ht] of stack)
+    maxArea = Math.max(maxArea, ht * (N - idx));
   
   return maxArea;
 };
