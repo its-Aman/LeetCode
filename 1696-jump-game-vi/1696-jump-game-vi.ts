34049e83d5c631b5061f8ab285733331301708cf
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-11 06:52:09 +0530

    Time: 152 ms (75.00%), Space: 56.9 MB (62.50%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.ts
+++ 1696-jump-game-vi/1696-jump-game-vi.ts
@@ -3,14 +3,17 @@ function maxResult(nums: number[], K: number): number {
         dp = new Array<number>(N),
         deque = new Array<number>(0);
   
+  let j = 0;
+  
   dp[N - 1] = nums[N - 1];
   deque.push(N - 1);
   
   for(let i = N - 2; i >= 0; i--) {
-    if(deque[0] - i > K)
-      deque.shift();
+    if(deque[j] - i > K)
+      j++;
+      // deque.shift();
     
-    dp[i] = nums[i] + dp[deque[0]];
+    dp[i] = nums[i] + dp[deque[j]];
     
     while(deque.length && dp[deque[deque.length - 1]] < dp[i])
       deque.pop();
