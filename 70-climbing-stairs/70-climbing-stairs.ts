8f691ab708ae0aac48032ca2c9798a1f1b54f095
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-13 20:46:57 +0530

    Time: 131 ms (5.24%), Space: 42.7 MB (66.04%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.ts
+++ 70-climbing-stairs/70-climbing-stairs.ts
@@ -1,4 +1,19 @@
 function climbStairs(N: number): number {
+  // return climbStairs_bottom_up(N);
+  return climbStairs_top_down(N);
+}
+
+function climbStairs_top_down(N: number, memo = new Map<number, number>()): number {
+  if(N < 4)
+    return N;
+  
+  if(!memo.has(N))
+    memo.set(N, climbStairs_top_down(N - 1, memo) + climbStairs_top_down(N - 2, memo));
+
+  return memo.get(N);
+}
+
+function climbStairs_bottom_up(N: number): number {
   if(N < 4)
     return N;
   
