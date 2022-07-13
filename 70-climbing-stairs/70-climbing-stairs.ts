9d1a64e5b5cd4665c0ac6728869fc711d8dde520
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-13 20:40:34 +0530

    Time: 113 ms (19.83%), Space: 42.9 MB (56.03%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.ts
+++ 70-climbing-stairs/70-climbing-stairs.ts
@@ -3,15 +3,13 @@ function climbStairs(N: number): number {
     return N;
   
   const dp = new Array<number>(N + 1);
+  
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
 
-  for(let i = 0; i <= N; i++){
-      
-    if(i < 4) {
-      dp[i] = i;
-    } else {
-      dp[i] = dp[i - 1] + dp[i - 2];
-    }
-  }
+  for(let i = 3; i <= N; i++)
+    dp[i] = dp[i - 1] + dp[i - 2];
   
   return dp[N];
 };
