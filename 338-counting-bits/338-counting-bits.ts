f05320b41b69cd22a187ecaf3344a1e6d8c43c52
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-15 07:46:40 +0530

    Time: 108 ms (85.28%), Space: 49.5 MB (64.50%) - LeetHub

--- 338-counting-bits/338-counting-bits.ts
+++ 338-counting-bits/338-counting-bits.ts
@@ -2,7 +2,7 @@ function countBits(N: number): number[] {
   const ans = new Array(N + 1).fill(0);
   
   for(let i = 0; i <= N; i++)
-    ans[i] = ans[i >> 1] + i % 2;
+    ans[i] = ans[i >> 1] + (i & 1);
       
   return ans;
 };
