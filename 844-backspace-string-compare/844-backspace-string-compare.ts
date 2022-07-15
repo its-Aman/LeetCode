58b57cd3f238ae3539b78b3ccd693981e3ec4318
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-15 06:56:26 +0530

    Time: 115 ms (29.96%), Space: 43.4 MB (74.90%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.ts
+++ 844-backspace-string-compare/844-backspace-string-compare.ts
@@ -1,18 +1,44 @@
-function backspaceCompare(s: string, t: string): boolean {
-  const s1 = new Array(0),
-        s2 = new Array(0);
+function backspaceCompare(S: string, T: string): boolean {
+  let i = S.length - 1,
+      j = T.length - 1,
+      skipS = 0,
+      skipT = 0;
   
-  for(let c of s)
-    if(c == '#')
-      s1.pop();
-    else
-      s1.push(c);
-
-  for(let c of t)
-    if(c == '#')
-      s2.pop();
-    else
-      s2.push(c);
-
-  return s1.join('') == s2.join('');
-};
+  while(i >= 0 || j >= 0) {
+    
+    while(i >= 0) {
+      if(S.at(i) == '#') {
+        skipS += 1;
+        i -= 1;
+      } else if(skipS > 0) {
+        skipS -= 1;
+        i -= 1;
+      } else {
+        break;
+      }
+    }
+    
+    while(j >= 0) {
+      if(T.at(j) == '#') {
+        skipT += 1;
+        j -= 1;
+      } else if(skipT > 0) {
+        skipT -= 1;
+        j -= 1;
+      } else {
+        break;
+      }
+    }
+    
+    if(i >= 0 && j >= 0 && S.at(i) != T.at(j))
+      return false;
+    
+    if((i >= 0) != (j >= 0))
+      return false;
+    
+    i -= 1;
+    j -= 1;
+  }
+  
+  return true;
+}
