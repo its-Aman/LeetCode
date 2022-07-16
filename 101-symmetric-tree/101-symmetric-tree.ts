2d74fc21f17e06b96d5337acc6245a415ed12f5e
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-16 12:12:46 +0530

    Time: 137 ms (17.28%), Space: 44.9 MB (76.64%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+
+  return dfs(root.left, root.right);
+  
+  function dfs(left: TreeNode | null, right: TreeNode | null): boolean {
+    if(!left && !right)
+      return true;
+    
+    if((!left && right) || (left && !right) || (left.val != right.val) || (left.left && !right.right) || (!left.left && right.right) || (left.right && !right.left) || (!left.right && right.left))
+      return false;
+    
+    return dfs(left.left, right.right) && dfs(left.right, right.left);
+  }
+  
+};
