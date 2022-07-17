12a878740fca736b8d6177fa93f995ab28ed4870
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-17 11:10:17 +0530

    Time: 214 ms (100.00%), Space: 56.6 MB (100.00%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.ts
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.ts
@@ -1,4 +1,24 @@
 function minOperations(nums: number[], numsDivide: number[]): number {
+  const M = numsDivide.length,
+        N = nums.length,
+        gcdNum = numsDivide.reduce((acc, curr) => gcd(curr, acc));
+  nums.sort((a, b) => a - b);
+
+  for(let i = 0; i < N; i++)
+    if(gcdNum % nums[i] == 0)
+      return i;
+  
+  return -1;
+  
+  function gcd(a: number, b: number): number {    
+    while(b)
+      [a, b] = [b, a % b];
+
+    return a;
+  }
+}
+
+function minOperations_old(nums: number[], numsDivide: number[]): number {
   const N = nums.length;
   
   nums.sort((a, b) => a - b);
