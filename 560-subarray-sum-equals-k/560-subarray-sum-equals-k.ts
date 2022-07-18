dc57a9f79c3875610f128ab4719f493a7ef38541
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-18 22:01:17 +0530

    Time: 132 ms (77.60%), Space: 50.8 MB (70.40%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.ts
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.ts
@@ -1,20 +1,23 @@
 function subarraySum(nums: number[], K: number): number {
   const N = nums.length,
-        dp = new Array(N + 1).fill(0);
-  let ans = 0;
+        map = new Map<number, number>();
+  let ans = 0,
+      prefix = 0;
   
-  for(let i = 0; i < N; i++)
-    dp[i + 1] = dp[i] + nums[i];
+  map.set(0, 1);
+  
+  for(let i = 0; i < N; i++) {
+    prefix += nums[i];
+    const left = prefix - K;
+    
+    if(map.has(left))
+      ans += map.get(left);
+    
+    if(map.has(prefix))
+      map.set(prefix, 1 + map.get(prefix));
+    else
+      map.set(prefix, 1);
+  }
 
-  for(let i = 1; i <= N; i++)
-    for(let j = i - 1; j >= 0; j--) {
-      const diff = dp[i] - dp[j];
-      if(diff == K)
-        ans += 1;
-      
-      // if(diff > K)
-      //   break;
-    }
-      
   return ans;
 };
