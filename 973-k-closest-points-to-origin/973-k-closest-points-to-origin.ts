7445fad36e351104834934f439aca0d2fb214ecb
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-24 20:11:21 +0530

    Time: 421 ms (31.30%), Space: 61.5 MB (49.19%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.ts
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.ts
@@ -1,38 +1,23 @@
 function kClosest(points: number[][], K: number): number[][] {
-  const N = points.length;
-  let lo = 0, 
-      hi = N - 1;
+  const N = points.length,
+        mpq = new MaxPriorityQueue({priority: x => x}),
+        ans = new Array(0);
   
-  while(lo <= hi) {
-    const mid = helper(lo, hi);
-    if(mid == K)
-      break;
-    else if(mid < K)
-      lo = mid + 1;
-    else
-      hi = mid - 1;
-  }
-  
-  return points.splice(0, K);
-  
-  function helper(lo: number, hi: number): number {
-    const pivot = points[lo];
+  for(let i = 0; i < N; i++) {
+    const dist = getDistance(points[i][0], points[i][1]);
+    mpq.enqueue(i, dist);
     
-    while(lo < hi) {
-      while(lo < hi && compare(points[hi], pivot) >= 0)
-        hi--;
-      points[lo] = points[hi];
+    if(mpq.size() > K)
+      mpq.dequeue();
+  }
 
-      while(lo < hi && compare(points[lo], pivot) <= 0)
-        lo++;
-      points[hi] = points[lo];      
-    }
-    
-    points[lo] = pivot;
-    return lo;
+  while(mpq.size())
+    ans.push(points[mpq.dequeue().element]);
+  
+  return ans;
+  
+  function getDistance(x: number, y: number): number {
+    return ((x * x) + (y * y)) ^ 0.5;
   }
   
-  function compare([x1, y1]: Array<number>, [x2, y2]: Array<number>): number {
-    return (x1 * x1 - x2 * x2) + (y1 * y1 - y2 * y2);
-  }
 };
