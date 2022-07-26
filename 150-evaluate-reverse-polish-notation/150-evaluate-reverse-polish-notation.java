b7477ccfd3e7b018d096032f7d345652836870c3
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-26 16:37:48 +0530

    Time: 8 ms (66.83%), Space: 44.8 MB (19.72%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,29 +1,26 @@
+interface Operationable {
+	int op(int n1, int n2);
+}
+
 class Solution {
-    public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
+	public int evalRPN(String[] tokens) {
+		HashMap<String, Operationable> map = new HashMap<>();
+		Stack<Integer> stack = new Stack<>();
 
-    for(String token: tokens) {
-      if(Arrays.asList("+", "-", "*", "/").contains(token)){
-        int ans = applyOps(stack.pop(), stack.pop(), token);
-        stack.push(ans);
-      } else {
-        stack.push(Integer.parseInt(token));
-      }
-    }
-    
-    return stack.pop();
-  }
+		map.put("+", (b, a) -> a + b);
+		map.put("-", (b, a) -> a - b);
+		map.put("/", (b, a) -> a / b);
+		map.put("*", (b, a) -> a * b);
 
-  public int applyOps(int num1, int num2, String op) {
-    int ans = 0;
-    switch(op) {
-      case "+": ans = num2 + num1; break;
-      case "-": ans = num2 - num1; break;
-      case "/": ans = num2 / num1; break;
-      case "*": ans = num2 * num1; break;
-    }
-    return ans;
-  }
+		for (String token : tokens) {
+			if ("+-*/".contains(token)) {
+				int ans = map.get(token).op(stack.pop(), stack.pop());
+				stack.push(ans);
+			} else {
+				stack.push(Integer.parseInt(token));
+			}
+		}
 
-  
+		return stack.pop();
+	}
 }
