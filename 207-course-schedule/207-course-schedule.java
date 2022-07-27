7ceb8a618af724d3b6c19ff0e22239a912ab18e9
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-27 06:23:46 +0530

    Time: 9 ms (54.12%), Space: 49 MB (33.52%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,52 @@
+class Solution {
+
+	Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+	Stack<Integer> stack = new Stack<Integer>();
+	boolean[] seen;
+
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		seen = new boolean[numCourses];
+
+		this.graph.clear();
+		this.stack.clear();
+
+		this.buildGraph(numCourses, prerequisites);
+
+		for (int i = 0; i < numCourses; i++) {
+			if (this.hasCycle(i)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private void buildGraph(int numCourses, int[][] prerequisites) {
+		for (int i = 0; i < numCourses; i++) {
+			this.graph.put(i, new ArrayList<Integer>());
+		}
+
+		for (int[] item : prerequisites) {
+			this.graph.get(item[0]).add(item[1]);
+		}
+	}
+
+	private boolean hasCycle(int v) {
+
+		if (this.seen[v]) {
+			return this.stack.contains(v);
+		}
+
+		this.seen[v] = true;
+		this.stack.push(v);
+
+		for (int item : this.graph.get(v)) {
+			if (this.hasCycle(item)) {
+				return true;
+			}
+		}
+
+		this.stack.pop();
+		return false;
+	}
+}
