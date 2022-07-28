315b9c051592c7489cc205588d37aee59150475a
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-28 07:36:14 +0530

    Time: 4 ms (76.56%), Space: 57.3 MB (52.55%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,33 +1,30 @@
 class Solution {
-	int ans = 0;
 
 	public int numIslands(char[][] grid) {
+		int ans = 0;
 
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid[i].length; j++) {
 				if (grid[i][j] == '1') {
-					this.ans += 1;
+					ans += 1;
 					this.bfs(grid, i, j);
 				}
 			}
 		}
 
-		return this.ans;
+		return ans;
 	}
 
 	public void bfs(char[][] grid, int i, int j) {
-		if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
+
+		if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1')
 			return;
 
-		int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 		grid[i][j] = '0';
 
-		for (int k = 0; k < dirs.length; k++) {
-			int dx = i + dirs[k][0];
-			int dy = j + dirs[k][1];
-
-			this.bfs(grid, dx, dy);
-		}
+		this.bfs(grid, i + 1, j);
+		this.bfs(grid, i - 1, j);
+		this.bfs(grid, i, j + 1);
+		this.bfs(grid, i, j - 1);
 	}
-
 }
