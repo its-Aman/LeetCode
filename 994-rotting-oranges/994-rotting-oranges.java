77bd284abb5378b08a11ccc37bd0c5e074ffa893
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-29 05:47:10 +0530

    Time: 2 ms (96.16%), Space: 42.9 MB (57.80%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,47 +1,43 @@
 class Solution {
-	public int orangesRotting(int[][] grid) {
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				if (grid[i][j] == 2) {
-					this.bfs(grid, i, j, 2);
-				}
-			}
-		}
+  public void dfs(int[][] grid, int i, int j, int minutes) {
+    if(
+      (i < 0 || i >= grid.length)
+      || (j < 0 || j >= grid[0].length)
+      || (grid[i][j] == 0)
+      || (grid[i][j] > 1 && grid[i][j] < minutes)
+    )
+      return;
+    
+    grid[i][j] = minutes;
+    
+    dfs(grid, i + 1, j, 1 + minutes);
+    dfs(grid, i - 1, j, 1 + minutes);
+    dfs(grid, i, j + 1, 1 + minutes);
+    dfs(grid, i, j - 1, 1 + minutes);
+  }
+  
+  public int orangesRotting(int[][] grid) {
+    
+    for(int i = 0; i < grid.length; i++) {
+      for(int j = 0; j < grid[0].length; j++) {
+        if(grid[i][j] == 2) {
+          dfs(grid, i, j, 2);
+        }
+      }
+    }
+      
+    int minutes = 2;
 
-		int minutes = 2;
+    for(int[] rows: grid) {
+      for(int cell: rows) {
+        if(cell == 1){
+          return -1;
+        } else {
+          minutes = Math.max(minutes, cell);
+        }
+      }
+    }
 
-		for (int[] row : grid) {
-			for (int cell : row) {
-				if (cell == 1)
-					return -1;
-				minutes = Math.max(cell, minutes);
-			}
-		}
-
-		return minutes - 2;
-	}
-
-	public void bfs(int[][] grid, int i, int j, int minutes) {
-
-		if (
-				i < 0 || i >= grid.length 
-				|| j < 0 || j >= grid[0].length 
-				|| grid[i][j] == 0 
-				|| (grid[i][j] > 1 && grid[i][j] < minutes)
-			) {
-			return;
-		}
-
-		grid[i][j] = minutes;
-
-		int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-
-		for (int[] dir : dirs) {
-			int dx = i + dir[0];
-			int dy = j + dir[1];
-
-			this.bfs(grid, dx, dy, 1 + minutes);
-		}
-	}
-
+    return minutes - 2;
+  }
 }
