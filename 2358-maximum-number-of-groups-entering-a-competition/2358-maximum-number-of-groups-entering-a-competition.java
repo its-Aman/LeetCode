fe0d25b0f97bebb5985edb1d74f6ec5fd70f6b86
Aman Kumar <kumaraman7056@hotmail.com>
2022-07-31 10:50:51 +0530

    Time: 0 ms (100.00%), Space: 69.6 MB (42.86%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
@@ -1,10 +1,16 @@
+// https://leetcode.com/problems/maximum-number-of-groups-entering-a-competition/discuss/2357789/JavaC%2B%2BPython-One-line-O(1)
+
 class Solution {
+//     public int maximumGroups(int[] grades) {
+//       int K = 0;
+      
+//       while(K*(K+1)/2 <= grades.length)
+//         K++;
+      
+//       return K - 1;
+//     }
+
     public int maximumGroups(int[] grades) {
-      int K = 0;
-      
-      while(K*(K+1)/2 <= grades.length)
-        K++;
-      
-      return K - 1;
+      return (int)(Math.sqrt(2 * grades.length + 0.25) - 0.5);
     }
 }
