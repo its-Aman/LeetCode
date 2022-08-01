541b4dfd39170982978f7880816c382c537ccec5
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-01 07:34:42 +0530

    Time: 8 ms (94.77%), Space: 47.7 MB (86.35%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -3,28 +3,22 @@ class Solution {
 	public int[][] merge(int[][] intervals) {
 		int N = intervals.length;
 
-		ArrayList<ArrayList<Integer>> mergeIntervals = new ArrayList<ArrayList<Integer>>();
+		ArrayList<int[]> mergeIntervals = new ArrayList<>();
 
 		Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
 
 		for (int i = 1; i <= N; i++) {
-			Integer start = intervals[i - 1][0];
-			Integer end = intervals[i - 1][1];
+			int start = intervals[i - 1][0];
+			int end = intervals[i - 1][1];
 
 			while (i < N && (start <= intervals[i][0] && intervals[i][0] <= end)) {
 				end = Math.max(end, intervals[i][1]);
 				i += 1;
 			}
 
-			ArrayList<Integer> mergeInterval = new ArrayList<Integer>();
-			mergeInterval.add(start);
-			mergeInterval.add(end);
-			mergeIntervals.add(mergeInterval);
+			mergeIntervals.add(new int[] { start, end });
 		}
 
-		int[][] ans = mergeIntervals.stream().map(i -> i.stream().mapToInt(point -> point).toArray())
-				.toArray(int[][]::new);
-
-		return ans;
+		return mergeIntervals.toArray(new int[0][]);
 	}
 }
