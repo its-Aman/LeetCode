9881afaa178f6a17ed2bfc8bf02344ce7e59f592
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-02 20:30:55 +0530

    Time: 408 ms (17.23%), Space: 215.3 MB (72.01%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,32 +1,44 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+class Data {
+    String value;
+    int time;
+    
+    public Data(String value, int time){
+        this.value = value;
+        this.time = time;
+    }
+}
 
 class TimeMap {
-	private Map<String, TreeMap<Integer, String>> map;
+	private HashMap<String, List<Data>> map;
 
     public TimeMap() {
 		this.map = new HashMap<>();
     }
     
 	public void set(String key, String value, int timestamp) {
-		this.map.putIfAbsent(key, new TreeMap<>());
-		this.map.get(key).put(timestamp, value);
+		this.map.putIfAbsent(key, new ArrayList<>());
+		this.map.get(key).add(new Data(value, timestamp));
 	}
 
 	public String get(String key, int timestamp) {
-		TreeMap<Integer, String> valMap = this.map.get(key);
-		if (valMap == null) {
-			return "";
-		}
-
-		Integer floorKey = valMap.floorKey(timestamp);
-
-		if (floorKey == null) {
-			return "";
-		}
-
-		return valMap.get(floorKey);
+        if(!this.map.containsKey(key))
+            return "";
+        
+        // binary search
+        
+        List<Data> dataList = this.map.get(key);
+        int lo = 0;
+        int hi = dataList.size() - 1;
+        
+        while(lo < hi) {
+            int mid = lo + (hi - lo + 1) / 2;
+            if(dataList.get(mid).time <= timestamp)
+                lo = mid;
+            else
+                hi = mid - 1;
+        }
+        
+        return dataList.get(lo).time <= timestamp ? dataList.get(lo).value : "";
 	}
 }
 
