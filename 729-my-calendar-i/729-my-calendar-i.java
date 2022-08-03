c342cecb2d0ab6bff3e4a06014c99c416bcbcba5
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-03 08:20:43 +0530

    Time: 31 ms (71.88%), Space: 54.1 MB (78.49%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,47 +1,21 @@
 class MyCalendar {
-	TreeSet<Pair<Integer, Integer>> times;
+	TreeMap<Integer, Integer> times;
 
     public MyCalendar() {
-		this.times = new TreeSet<>((a, b) -> a.getKey() - b.getKey());
+		this.times = new TreeMap<>();
 	}
 
 	public boolean book(int start, int end) {
-		Pair<Integer, Integer> time = new Pair<Integer, Integer>(start, end);
+		Integer next = this.times.ceilingKey(start);
+		Integer prev = this.times.floorKey(start);
 
-		if (this.times.size() == 0) {
-			this.times.add(time);
-			return true;
-		}
-        
-		Pair<Integer, Integer> ceil = this.times.ceiling(time);
-		Pair<Integer, Integer> floor = this.times.floor(time);
-        
-		if (ceil == null) {
-			// check for floor
-			if (time.getKey() >= floor.getValue()) {
-				this.times.add(time);
-				return true;
-			} else {
-				return false;
-			}
-		}
-		if (floor == null) {
-			// check for ceil
-			if (time.getValue() <= ceil.getKey()) {
-				this.times.add(time);
-				return true;
-			} else {
-				return false;
-			}
-		}
-		if (time.getKey() >= floor.getValue() && time.getValue() <= ceil.getKey()) {
-			this.times.add(time);
+		if ((prev == null || this.times.get(prev) <= start) && (next == null || end <= next)) {
+			this.times.put(start, end);
 			return true;
 		} else {
 			return false;
 		}
-
-	}
+    }
 }
 
 /**
