e5fcb0c6075fa6ab07b30663dde58cdb16b8bf47
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-07 10:11:32 +0530

    Time: 176 ms (14.29%), Space: 201.6 MB (14.29%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.java
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.java
@@ -1,26 +1,20 @@
 class Solution {
 	public int reachableNodes(int n, int[][] edges, int[] restricted) {
 		int ans = 0;
-		List<Integer>[] adjList = new ArrayList[n];
+		List<List<Integer>> adjList = new ArrayList<>();
+		Set<Integer> set = new HashSet<>();
+		Queue<Integer> q = new LinkedList<>();
+
+		for (int r : restricted)
+			set.add(r);
+
+		for (int i = 0; i < n; i++)
+			adjList.add(i, new ArrayList<Integer>());
 
 		for (int[] e : edges) {
-			if (adjList[e[0]] == null) {
-				adjList[e[0]] = new ArrayList<>();
-			}
-			adjList[e[0]].add(e[1]);
-
-			if (adjList[e[1]] == null) {
-				adjList[e[1]] = new ArrayList<>();
-			}
-			adjList[e[1]].add(e[0]);
+			adjList.get(e[0]).add(e[1]);
+			adjList.get(e[1]).add(e[0]);
 		}
-
-		Set<Integer> set = new HashSet<>();
-
-		for (int dnd : restricted)
-			set.add(dnd);
-
-		Queue<Integer> q = new LinkedList<>();
 
 		q.add(0);
 		set.add(0);
@@ -29,10 +23,10 @@ class Solution {
 
 			int sz = q.size();
 
-			for (int i = 0; i < sz; i++) {
+			while (sz-- > 0) {
 				ans += 1;
-				int curr = q.poll();
-				for (int next : adjList[curr]) {
+				Integer curr = q.poll();
+				for (int next : adjList.get(curr)) {
 					if (set.add(next)) {
 						q.add(next);
 					}
