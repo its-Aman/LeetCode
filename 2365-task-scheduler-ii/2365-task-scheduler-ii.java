2c4b878843b122bf0ba0344f4b36dd01f42ddb80
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-07 07:18:14 +0530

    Time: 71 ms (44.44%), Space: 138.1 MB (11.11%) - LeetHub

---
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/task-scheduler-ii/discuss/2388880/C%2B%2B-Solution-Hindi-explanation-MAP
+
+class Solution {
+    public long taskSchedulerII(int[] tasks, int space) {
+        long currTime = 0;
+        Map<Integer, Long> map = new HashMap<>();
+        
+        for(int i = 0; i < tasks.length; i++) {
+                
+            if(map.containsKey(tasks[i]) && map.get(tasks[i]) > currTime) {
+                currTime = map.get(tasks[i]);                
+            }
+            
+            map.put(tasks[i], 1 + currTime + space);
+            currTime += 1;
+            
+            // map.put(tasks[i], 1 + space + (currTime = Math.max(currTime, map.getOrDefault(tasks[i], currTime))));
+            // currTime += 1;
+
+        }
+
+        
+        return currTime;
+    }
+}
