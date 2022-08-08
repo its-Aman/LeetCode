9cb7f4067b9238ded4e1d9f5efcca425a6109034
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-08 08:22:44 +0530

    Time: 3 ms (98.97%), Space: 45.3 MB (16.97%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,49 +1,38 @@
 class Solution {
-//     int[] dp;
-
-//     public int lengthOfLIS_Recursive(int[] nums) {
-//         this.dp = new int[1 + nums.length];
-//         Arrays.fill(dp, -1);
-        
-//         return this.solve(nums, 0, -1);
-//     }
-    
-//     private int solve(int[] nums, int idx, int prev_i) {
-//         if(idx >= nums.length)
-//             return 0;
-
-//         if(this.dp[prev_i + 1] != -1)
-//             return this.dp[prev_i + 1];
-        
-//         int take = 0;
-//         int dontTake = this.solve(nums, idx + 1, prev_i);
-        
-//         if(prev_i == -1 || nums[idx] > nums[prev_i])
-//             take = 1 + this.solve(nums, idx + 1, idx);
-        
-//         return this.dp[prev_i + 1] = Math.max(take, dontTake);
-//     }
-    
-    public int lengthOfLIS(int[] nums) {
+	public int lengthOfLIS(int[] nums) {
 		int N = nums.length;
-		int[] dp = new int[1 + nums.length];
-		Arrays.fill(dp, 1);
-		int ans = 1;
+		var list = new int[N];
+		var idx = 0;
 
-		for (int i = 0; i < N; i++) {
-
-			for (int j = 0; j < i; j++) {
-
-				if (nums[i] > nums[j]) {
-					dp[i] = Math.max(1 + dp[j], dp[i]);
-
-					ans = Math.max(ans, dp[i]);
-				}
-
+		for (int num : nums) {
+			if (idx == 0 || list[idx - 1] < num) {
+				list[idx++] = num;
+				continue;
 			}
 
+			int j = this.binarySearch_LowerBound(list, idx, num);
+			list[j] = num;
 		}
-        
-        return ans;
+
+		return idx;
+	}
+
+	private int binarySearch_LowerBound(int[] list, int hi, int num) {
+		int lo = 0;
+
+		while (lo < hi) {
+			int mid = lo + (hi - lo) / 2;
+
+			if (num <= list[mid]) {
+				hi = mid;
+			} else {
+				lo = mid + 1;
+			}
+		}
+
+		if (lo < list.length && list[lo] < num)
+			lo += 1;
+
+		return lo;
 	}
 }
