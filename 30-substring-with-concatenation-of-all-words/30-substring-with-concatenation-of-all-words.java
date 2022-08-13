da71c3105bb1630c2dbc988add0a5b5eb4ce4dd1
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-13 10:59:03 +0530

    Time: 219 ms (53.90%), Space: 68 MB (59.71%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -1,21 +1,47 @@
 class Solution {
+	int wordCount;
 	int wordLen;
 	int totaWordlLen;
+	Map<String, Integer> map;
 
 	public List<Integer> findSubstring(String S, String[] words) {
 		LinkedList<Integer> ans = new LinkedList<>();
-		Map<String, Integer> map = new HashMap<>();
-
+		this.map = new HashMap<>();
+		this.wordCount = words.length;
 		this.wordLen = words[0].length();
-		this.totaWordlLen = wordLen * words.length;
+		this.totaWordlLen = this.wordLen * this.wordCount;
 
 		for (String word : words)
 			map.put(word, 1 + map.getOrDefault(word, 0));
 
-		for (int i = 0; i <= S.length() - this.totaWordlLen; i++)
-			if (this.helper(i, S, new HashMap<>(map)))
+		for (int i = 0; i < S.length() - this.totaWordlLen + 1; i++) {
+			if (this.slidingWindow(i, S))
+			// if (this.helper(i, S, new HashMap<>(this.map)))
 				ans.add(i);
+		}
+
 		return ans;
+	}
+
+	private boolean slidingWindow(int start, String S) {
+		final Map<String, Integer> seen = new HashMap<String, Integer>();
+		int j = 0;
+
+		while (j < this.wordCount) {
+			String word = S.substring(start + j * this.wordLen, start + (j + 1) * this.wordLen);
+
+			if (!this.map.containsKey(word))
+				break;
+
+			seen.put(word, seen.getOrDefault(word, 0) + 1);
+
+			if (seen.get(word) > this.map.getOrDefault(word, 0))
+				break;
+
+			j += 1;
+		}
+
+		return j == this.wordCount;
 	}
 
 	private boolean helper(int start, String S, Map<String, Integer> localHashTable) {
