346c112ed2dd8796aa04ab80773db48ebf317c3f
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-14 14:29:32 +0530

    Time: 3 ms (78.49%), Space: 47.9 MB (79.40%) - LeetHub

--- 942-di-string-match/942-di-string-match.java
+++ 942-di-string-match/942-di-string-match.java
@@ -1,19 +1,15 @@
 class Solution {
 	public int[] diStringMatch(String S) {
 		int N = S.length();
-		int j = 0;
 		int[] ans = new int[N + 1];
-		Stack<Integer> stack = new Stack<>();
+        int lo = 0;
+        int hi = N;
 
 		for (int i = 0; i <= N; i++) {
-			stack.push(i);
-
-			if (i == N || S.charAt(i) == 'I') {
-
-				while (!stack.isEmpty())
-					ans[j++] = stack.pop();
-
-			}
+			if (i == N || S.charAt(i) == 'I')
+                ans[i] = lo++;
+            else
+                ans[i] = hi--;
 		}
 
 		return ans;
