1688d50387b5e71dbbf9539bc19a8f5a333194fa
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-16 12:49:18 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (28.07%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -14,7 +14,30 @@
  * }
  */
 class Solution {
+    private int k;
+    private int ans = -1;
+    
     public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        this.inorder(root);
+        
+        return this.ans;
+    }
+    
+    private void inorder(TreeNode node) {
+        if(node == null)
+            return;
+        
+        this.inorder(node.left);
+        
+        if(--this.k == 0)
+            this.ans = node.val;
+        
+        if(this.ans == -1)
+            this.inorder(node.right);
+    }
+    
+    public int kthSmallest_recursive(TreeNode root, int k) {
         LinkedList<TreeNode> stack = new LinkedList<>();
         
         stack.push(root);
