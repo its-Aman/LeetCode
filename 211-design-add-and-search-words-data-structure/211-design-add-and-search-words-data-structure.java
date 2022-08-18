2f5034610276624c8d67ca94e545e7e19d0a16e4
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-18 06:11:48 +0530

    Time: 1096 ms (53.06%), Space: 272 MB (47.76%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,62 @@
+class WordDictionary {
+
+	class TrieNode {
+		public TrieNode[] next = new TrieNode[26];
+		public boolean isEnd;
+	}
+
+	private TrieNode root;
+
+	public WordDictionary() {
+		this.root = new TrieNode();
+	}
+
+	public void addWord(String word) {
+		TrieNode curr = this.root;
+
+		for (char c : word.toCharArray()) {
+			if (curr.next[c - 'a'] == null) {
+				curr.next[c - 'a'] = new TrieNode();
+			}
+			curr = curr.next[c - 'a'];
+		}
+		curr.isEnd = true;
+	}
+
+	public boolean search(String word) {
+		return this.search(word, this.root, 0);
+	}
+
+	public boolean search(String word, TrieNode curr, int startsAt) {
+		boolean found = false;
+
+		for (int i = startsAt; i < word.length(); i++) {
+			if (word.charAt(i) == '.') {
+				for (int j = 0; j < 26; j++) {
+					if (curr.next[j] != null) {
+						found = found || this.search(word, curr.next[j], i + 1);
+					}
+				}
+                
+				if (!found)
+					return false;
+				else
+					break;
+            } else {
+				if (curr.next[word.charAt(i) - 'a'] == null)
+					return false;
+
+				curr = curr.next[word.charAt(i) - 'a'];
+			}
+		}
+
+		return curr.isEnd || found;
+	}
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
