2745aff9bc8283477f65cac0908c605e5a8e1189
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-19 22:34:52 +0530

    Time: 1 ms (100.00%), Space: 71.9 MB (40.00%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
@@ -1,38 +1,43 @@
 class Solution {
     
 	public boolean isPossible(int[] nums) {
-		int[] left = new int[2000 + 3];
-		int[] end = new int[2000 + 3];
+		int pre = Integer.MIN_VALUE, p1 = 0, p2 = 0, p3 = 0;
+		int curr = 0, cnt = 0, c1 = 0, c2 = 0, c3 = 0;
+		int i = 0, N = nums.length;
 
-		for (int num : nums)
-			left[1000 + 1 + num] += 1;
+		while (i < N) {
 
-		for (int num : nums) {
-			num += 1000 + 1;
+			curr = nums[i];
+			cnt = 0;
 
-			if (left[num] <= 0)
-				continue;
+			while (i < N && curr == nums[i]) {
+				cnt += 1;
+				i += 1;
+			}
 
-			left[num] -= 1;
+			if (curr != pre + 1) {
+				if (p1 != 0 || p2 != 0)
+					return false;
 
-			if (end[num - 1] > 0) {
-
-				end[num - 1] -= 1;
-				end[num] += 1;
-
-			} else if (left[num + 1] > 0 && left[num + 2] > 0) {
-
-				left[num + 1] -= 1;
-				left[num + 2] -= 1;
-				end[num + 2] += 1;
-
+				c1 = cnt;
+				c2 = 0;
+				c3 = 0;
 			} else {
+				if (cnt < p1 + p2)
+					return false;
 
-				return false;
+				c1 = Math.max(0, cnt - (p1 + p2 + p3));
+				c2 = p1;
+				c3 = p2 + Math.min(p3, cnt - (p1 + p2));
 
 			}
+
+			pre = curr;
+			p1 = c1;
+			p2 = c2;
+			p3 = c3;
 		}
 
-		return true;
+		return (p1 == 0 && p2 == 0);
 	}
 }
