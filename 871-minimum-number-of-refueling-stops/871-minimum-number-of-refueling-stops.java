0f0d8f184cca03bd8b7b15e2b4be9c28dab89510
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-20 19:27:09 +0530

    Time: 4 ms (86.38%), Space: 49.4 MB (28.43%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,5 +1,25 @@
 class Solution {
+
 	public int minRefuelStops(int target, int startFuel, int[][] stations) {
+		return this.minRefuelStops_heap(target, startFuel, stations);
+	}
+
+	private int minRefuelStops_dp(int target, int startFuel, int[][] stations) {
+		/*
+		 * Approach 1: 1D DP, O(N^2)
+		 * 
+		 * dp[t] means the furthest distance that we can get with t times of refueling.
+		 * 
+		 * So for every station s[i], 
+		 * 
+		 * if the current distance dp[t] >= s[i][0], we can refuel: 
+		 *
+		 ** dp[t + 1] = max(dp[t + 1], dp[t] + s[i][1])
+		 * 
+		 * In the end, we'll return the first t with dp[t] >= target, 
+		 ** otherwise we'll return -1.
+		 */
+
 		int N = stations.length;
 		long[] dp = new long[N + 1];
 
@@ -15,15 +35,38 @@ class Solution {
 
 		return -1;
 	}
+
+	private int minRefuelStops_heap(int target, int startFuel, int[][] stations) {
+		/*
+		 * Approach 2: Priority Queue, O(NlogN)
+		 * 
+		 * i is the index of next stops to refuel. 
+		 * 
+		 * res is the times that we have refeuled. 
+		 * 
+		 * pq is a priority queue that we store all available gas.
+		 * 
+		 * 
+		 * We initial res = 0 and in every loop:
+		 * 
+		 ** We add all reachable stop to priority queue. 
+		 ** We pop out the largest gas from pq and refeul once. 
+		 ** If we can't refuel, means that we can not go forward and return -1
+		 */
+		int i = 0, res = 0, N = stations.length;
+		Queue<Integer> pq = new PriorityQueue<Integer>();
+
+		for (res = 0; startFuel < target; res += 1) {
+
+			while (i < stations.length && stations[i][0] <= startFuel)
+				pq.offer(-stations[i++][1]);
+
+			if (pq.isEmpty())
+				return -1;
+
+			startFuel += -pq.poll();
+		}
+
+		return res;
+	}
 }
-
-
-/*
-
-    N
-    
-W   +   E
-    
-    S
-
-*/
