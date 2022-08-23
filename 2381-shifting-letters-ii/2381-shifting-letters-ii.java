aeaed35be54971bab244cbb13031bc4296e74521
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-23 06:47:52 +0530

    Time: 20 ms (25.00%), Space: 103.7 MB (25.00%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.java
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -1,6 +1,47 @@
 class Solution {
     
-    	public String shiftingLetters(String S, int[][] shifts) {
+    public String shiftingLetters(String S, int[][] shifts) {
+        return this.shiftingLetters_fenwickTree(S, shifts);
+    }
+    
+	int N;
+	int[] bTree;
+
+	public String shiftingLetters_fenwickTree(String S, int[][] shifts) {
+		this.N = S.length();
+		this.bTree = new int[this.N + 1];
+		char[] c = S.toCharArray();
+
+		for (int[] s : shifts) {
+			int d = s[2] == 1 ? 1 : -1;
+			this.add(s[0], d);
+			this.add(s[1] + 1, -d);
+		}
+
+		for (int i = 0; i < this.N; i++) {
+			int increaseBy = (c[i] - 'a') + this.prefixSum(i) % 26;
+			int onlyPositiveMod = (increaseBy + 26) % 26;
+			c[i] = (char) ('a' + onlyPositiveMod);
+		}
+		
+		return new String(c);
+	}
+
+	public void add(int idx, int val) {
+		for (int i = idx + 1; i <= N; i += i & (-i))
+			this.bTree[i] += val;
+	}
+
+	public int prefixSum(int idx) {
+		int sum = 0;
+
+		for (int i = idx + 1; i > 0; i -= i & (-i))
+			sum += this.bTree[i];
+
+		return sum;
+	}
+
+	public String shiftingLetters_lineSweep(String S, int[][] shifts) {
 		char[] cs = S.toCharArray();
 		int[] line = new int[S.length() + 1];
 
@@ -18,33 +59,33 @@ class Solution {
 
 		return new String(cs);
 	}
-    
-// 	public String shiftingLetters(String S, int[][] shifts) {
-// 		char[] cs = S.toCharArray();
 
-// 		for (int[] shift : shifts) {
-// 			int s = shift[0];
-// 			int e = shift[1];
-// 			int d = shift[2];
+	public String shiftingLetters_bruteForce(String S, int[][] shifts) {
+		char[] cs = S.toCharArray();
 
-// 			while (s <= e) {
+		for (int[] shift : shifts) {
+			int s = shift[0];
+			int e = shift[1];
+			int d = shift[2];
 
-// 				if (d == 1) { // forward
-// 					if (cs[s] == 'z')
-// 						cs[s] = 'a';
-// 					else
-// 						cs[s] += 1;
-// 				} else { // backward
-// 					if (cs[s] == 'a')
-// 						cs[s] = 'z';
-// 					else
-// 						cs[s] -= 1;
-// 				}
+			while (s <= e) {
 
-// 				s += 1;
-// 			}
-// 		}
+				if (d == 1) { // forward
+					if (cs[s] == 'z')
+						cs[s] = 'a';
+					else
+						cs[s] += 1;
+				} else { // backward
+					if (cs[s] == 'a')
+						cs[s] = 'z';
+					else
+						cs[s] -= 1;
+				}
 
-// 		return new String(cs);
-// 	}
+				s += 1;
+			}
+		}
+
+		return new String(cs);
+	}
 }
