457d93757342da6c9b083fcd702d39fa419b979a
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-28 06:11:12 +0530

    Time: 8 ms (74.83%), Space: 43.5 MB (89.06%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -3,24 +3,16 @@ class Solution {
 		int R = M.length, C = M[0].length;
 		int[][] ans = new int[R][C];
 
-		Map<Integer, List<Integer>> map = new HashMap<>();
+		Map<Integer, Queue<Integer>> map = new HashMap<>();
 
 		for (int r = 0; r < R; r++)
 			for (int c = 0; c < C; c++)
-				map.computeIfAbsent(r - c, k -> new ArrayList<>()).add(M[r][c]);
+				map.computeIfAbsent(r - c, k -> new PriorityQueue<>()).add(M[r][c]);
 
-		for (List<Integer> l : map.values())
-			Collections.sort(l);
-
-		for (int r = R - 1; r >= 0; r--) {
-			for (int c = C - 1; c >= 0; c--) {
-				var l = map.get(r - c);
-				ans[r][c] = l.get(l.size() - 1);
-				l.remove(l.size() - 1);
-			}
-		}
+		for (int r = 0; r < R; r++)
+			for (int c = 0; c < C; c++)
+				ans[r][c] = map.get(r - c).poll();
 
 		return ans;
 	}
-
 }
