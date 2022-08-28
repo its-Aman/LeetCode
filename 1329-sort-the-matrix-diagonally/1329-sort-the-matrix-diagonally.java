6724875a3fe56eab581cc5048219c095da11ca8c
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-28 06:36:52 +0530

    Time: 1 ms (100.00%), Space: 48.6 MB (24.31%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,18 +1,37 @@
 class Solution {
-	public int[][] diagonalSort(int[][] M) {
+
+    public int[][] diagonalSort(int[][] M) {
 		int R = M.length, C = M[0].length;
 		int[][] ans = new int[R][C];
 
-		Map<Integer, Queue<Integer>> map = new HashMap<>();
+		for (int c = 0; c < C; c++)
+			this.countSort(M, ans, 0, c);
 
 		for (int r = 0; r < R; r++)
-			for (int c = 0; c < C; c++)
-				map.computeIfAbsent(r - c, k -> new PriorityQueue<>()).add(M[r][c]);
-
-		for (int r = 0; r < R; r++)
-			for (int c = 0; c < C; c++)
-				ans[r][c] = map.get(r - c).poll();
+			this.countSort(M, ans, r, 0);
 
 		return ans;
 	}
+
+	private void countSort(int[][] M, int[][] A, int R, int C) {
+		int[] cnt = new int[101];
+		int r = R, c = C;
+
+		while (r < A.length && c < A[0].length)
+			cnt[M[r++][c++]] += 1;
+
+		r = R;
+		c = C;
+
+		for (int i = 1; i < 101; i++) {
+			if (cnt[i] > 0) {
+				int val = cnt[i];
+
+				while (val-- > 0)
+					A[r++][c++] = i;
+			}
+		}
+	}
+
+    
 }
