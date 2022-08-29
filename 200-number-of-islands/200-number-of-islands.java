bb7873b10a0065be6e29d1194cb251cb7e5be185
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-29 09:22:17 +0530

    Time: 9 ms (23.73%), Space: 57.8 MB (33.07%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,39 +1,88 @@
-/*
-[
-    ["1","1","1"],
-    ["0","1","0"],
-    ["1","1","1"]
-]
-*/
 class Solution {
-    public int numIslands(char[][] grid) {
-        int R = grid.length, C = grid[0].length;
-        int ans = 0;
+	class UF {
+		public int count = 0;
+		public int[] id = null;
 
-        for(int r = 0; r < R; r++) {
-            for(int c = 0; c < C; c++) {
-                if(grid[r][c] == '1') {
-                    ans += 1;
-                    grid[r][c] = '0';
-                    this.dfs(grid, r, c);
-                }
-            }
-        }
-        
-        return ans;
-    }
-    
-    private void dfs(char[][] G, int r, int c) {
-        
-        int[] dirs = new int[]{-1, 0, 1, 0, -1};
-        
-        for(int d = 3; d >= 0; d--) {
-            int dr = r + dirs[d], dc = c + dirs[d + 1];
-            
-            if(dr >= 0 && dr < G.length && dc >= 0 &&  dc < G[0].length && G[dr][dc] == '1') {
-                G[dr][dc] = '0';
-                this.dfs(G, dr, dc);
-            }
-        }
-    }
+		public UF(int m, int n, char[][] grid) {
+
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if (grid[i][j] == '1')
+						count++;
+				}
+			}
+
+			id = new int[m * n];
+
+			for (int i = 0; i < m * n; i++)
+				id[i] = i;
+		}
+
+		public int find(int p) {
+
+			while (p != id[p]) {
+				id[p] = id[id[p]];
+				p = id[p];
+			}
+
+			return p;
+		}
+
+		public boolean isConnected(int p, int q) {
+			int pp = find(p);
+			int qq = find(q);
+
+			return pp == qq;
+		}
+
+		public void union(int p, int q) {
+			int pp = find(p);
+			int qq = find(q);
+
+			if (pp == qq)
+				return;
+
+			id[pp] = qq;
+
+			count--;
+		}
+	}
+
+	public int numIslands(char[][] grid) {
+		if (grid.length == 0 || grid[0].length == 0)
+			return 0;
+
+		int m = grid.length, n = grid[0].length;
+
+		UF uf = new UF(m, n, grid);
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+
+				if (grid[i][j] == '0')
+					continue;
+
+				int p = i * n + j, q;
+
+				if (i > 0 && grid[i - 1][j] == '1') {
+					q = p - n;
+					uf.union(p, q);
+				}
+
+				if (i < m - 1 && grid[i + 1][j] == '1') {
+					q = p + n;
+					uf.union(p, q);
+				}
+				if (j > 0 && grid[i][j - 1] == '1') {
+					q = p - 1;
+					uf.union(p, q);
+				}
+				if (j < n - 1 && grid[i][j + 1] == '1') {
+					q = p + 1;
+					uf.union(p, q);
+				}
+			}
+		}
+		return uf.count;
+	}
 }
