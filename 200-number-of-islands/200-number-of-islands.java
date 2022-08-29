66dbc62d14c805be25661b99c4215851ff24b853
Aman Kumar <kumaraman7056@hotmail.com>
2022-08-29 07:52:35 +0530

    Time: 5 ms (55.04%), Space: 57 MB (68.64%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,30 +1,39 @@
+/*
+[
+    ["1","1","1"],
+    ["0","1","0"],
+    ["1","1","1"]
+]
+*/
 class Solution {
+    public int numIslands(char[][] grid) {
+        int R = grid.length, C = grid[0].length;
+        int ans = 0;
 
-	public int numIslands(char[][] grid) {
-		int ans = 0;
-
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				if (grid[i][j] == '1') {
-					ans += 1;
-					this.bfs(grid, i, j);
-				}
-			}
-		}
-
-		return ans;
-	}
-
-	public void bfs(char[][] grid, int i, int j) {
-
-		if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1')
-			return;
-
-		grid[i][j] = '0';
-
-		this.bfs(grid, i + 1, j);
-		this.bfs(grid, i - 1, j);
-		this.bfs(grid, i, j + 1);
-		this.bfs(grid, i, j - 1);
-	}
+        for(int r = 0; r < R; r++) {
+            for(int c = 0; c < C; c++) {
+                if(grid[r][c] == '1') {
+                    ans += 1;
+                    grid[r][c] = '0';
+                    this.dfs(grid, r, c);
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    private void dfs(char[][] G, int r, int c) {
+        
+        int[] dirs = new int[]{-1, 0, 1, 0, -1};
+        
+        for(int d = 3; d >= 0; d--) {
+            int dr = r + dirs[d], dc = c + dirs[d + 1];
+            
+            if(dr >= 0 && dr < G.length && dc >= 0 &&  dc < G[0].length && G[dr][dc] == '1') {
+                G[dr][dc] = '0';
+                this.dfs(G, dr, dc);
+            }
+        }
+    }
 }
