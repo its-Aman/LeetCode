38cde05f48459c029eb3fe82210e46fc5c8c4181
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-01 19:05:07 +0530

    Time: 9 ms (91.30%), Space: 57.7 MB (78.38%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
@@ -10,18 +10,17 @@ class Solution {
 
         int ans = 0, N = nums.length;
         int[] cnt = new int[N + 1];
-        int left = 0, uniq = 0;
 
-        for (int right = 0; right < N; right++) {
-            if (cnt[nums[right]] == 0) 
-                uniq += 1;
-            
+        for (int left = 0, right = 0; right < N; right++) {
+            if (cnt[nums[right]] == 0)
+                K -= 1;
+
             cnt[nums[right]] += 1;
 
-            while (right < N && left <= right && uniq > K) {
+            while (K < 0) {
                 cnt[nums[left]] -= 1;
                 if (cnt[nums[left]] == 0)
-                    uniq -= 1;
+                    K += 1;
                 left += 1;
             }
             ans += right - left + 1;
