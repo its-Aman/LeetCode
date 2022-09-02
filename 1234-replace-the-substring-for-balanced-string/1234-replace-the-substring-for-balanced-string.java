103a79e464a21a05d887382136987b719d790bac
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-02 20:50:18 +0530

    Time: 61 ms (16.37%), Space: 43.9 MB (61.06%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.java
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.java
@@ -1,35 +1,21 @@
 class Solution {
     public int balancedString(String S) {
-        int N = S.length(), ans = N + 1;
-        var map = new HashMap<Character, Integer>();
-        map.put('Q', 0);
-        map.put('W', 1);
-        map.put('E', 2);
-        map.put('R', 3);
-        int[] inWindowCount = new int[4];
-        int[] outWindowCount = new int[4];
+        int N = S.length(), ans = N;
+        int[] count = new int[128];
 
         for (int i = 0; i < N; i++)
-            outWindowCount[map.get(S.charAt(i))]++;
-
-        if(checkValid(outWindowCount, N/4))
-            return 0;
+            ++count[S.charAt(i)];
 
         for (int left = 0, right = 0; right < N; right++) {
-            int r = map.get(S.charAt(right));
-            inWindowCount[r]++;
-            outWindowCount[r]--;
+            --count[S.charAt(right)];
 
-            while (checkValid(outWindowCount, N / 4)) {
+            while (left < N && checkValid(count, N / 4)) {
                 ans = Math.min(ans, right - left + 1);
-                int l = map.get(S.charAt(left++));
-                inWindowCount[l] -= 1;
-                outWindowCount[l] += 1;
+                ++count[S.charAt(left++)];
             }
-
         }
 
-        return ans > N ? 0 : ans;
+        return ans;
     }
 
     private boolean checkValid(int[] count, int K) {
@@ -39,5 +25,4 @@ class Solution {
 
         return true;
     }
-
 }
