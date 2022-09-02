cb87f3b221c9250c7da2185fa97e7b3a00b1ec89
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-02 20:54:29 +0530

    Time: 6 ms (99.11%), Space: 41.8 MB (100.00%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.java
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public int balancedString(String S) {
-        int N = S.length(), ans = N;
+        int N = S.length(), ans = N, K = N / 4;
         int[] count = new int[128];
 
         for (int i = 0; i < N; i++)
@@ -9,20 +9,12 @@ class Solution {
         for (int left = 0, right = 0; right < N; right++) {
             --count[S.charAt(right)];
 
-            while (left < N && checkValid(count, N / 4)) {
+            while (left < N && count['Q'] <= K && count['W'] <= K && count['E'] <= K && count['R'] <= K) {
                 ans = Math.min(ans, right - left + 1);
                 ++count[S.charAt(left++)];
             }
         }
 
         return ans;
-    }
-
-    private boolean checkValid(int[] count, int K) {
-        for (int c : count)
-            if (c > K)
-                return false;
-
-        return true;
     }
 }
