b6447b0f74bad82f9b00ef4fc3b22afd95291515
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-03 13:21:58 +0530

    Time: 3 ms (80.94%), Space: 42.6 MB (72.81%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,62 +1,37 @@
 class Solution {
-    Set<Long> uniq = new HashSet<>();
+    List<Integer> ans = new ArrayList<>();
 
     public int[] numsSameConsecDiff(int N, int K) {
-        this.backTrack(new int[N], 0, K);
+
+        for (int i = 1; i < 10; i++)
+            this.dfs(N, K, i, 0, i);
+
         return this.toIntArray();
     }
+    
+    private void dfs(int N, int K, int curr, int idx, int prev) {
 
-    private void backTrack(int[] num, int idx, int K) {
-        var n = this.toNum(num);
-        if (this.uniq.contains(n))
-            return;
-
-        if (idx == num.length) {
-            this.uniq.add(n);
+        if (idx == N - 1) {
+            this.ans.add(curr);
             return;
         }
 
-        for (int i = 0; i < 10; i++) {
-            if (idx == 0 && i == 0)
-                continue;
+        int next = prev + K;
+        if (next < 10)
+            this.dfs(N, K, (curr * 10) + next, idx + 1, next);
 
-            if (idx == 0) {
-                num[idx] = i;
-                this.backTrack(num, idx + 1, K);
-            } else {
-                int prev = num[idx - 1];
-                if (prev - K >= 0) {
-                    num[idx] = prev - K;
-
-                    this.backTrack(num, idx + 1, K);
-                }
-                if (prev + K < 10) {
-                    num[idx] = prev + K;
-                    this.backTrack(num, idx + 1, K);
-                }
-            }
-        }
+        next = prev - K;
+        if (K != 0 && next >= 0)
+            this.dfs(N, K, (curr * 10) + next, idx + 1, next);
     }
+
 
     private int[] toIntArray() {
-        var res = new int[this.uniq.size()];
-        var i = 0;
-
-        var it = this.uniq.iterator();
-
-        while (it.hasNext())
-            res[i++] = it.next().intValue();
-
+        var res = new int[this.ans.size()];
+        
+        for(int i = 0; i < this.ans.size(); i++)
+            res[i] = this.ans.get(i);
         return res;
-    }
-
-    private long toNum(int[] num) {
-        long n = 0;
-        for (var d : num) {
-            n += d;
-            n *= 10;
-        }
-        return n / 10;
     }
 
 }
