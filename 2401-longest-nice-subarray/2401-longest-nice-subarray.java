ecb440cf38c1205444514d40bf1dea7ba2ff0091
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-04 14:04:41 +0530

    Time: 3 ms (100.00%), Space: 78.9 MB (50.00%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int N = nums.length, ans = 0, mask = 0;
+
+        for (int l = 0, r = 0; r < N; r++) {
+            while ((mask & nums[r]) != 0)
+                mask ^= nums[l++];
+            mask |= nums[r];
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+}
