068817a60195f800731ab42b958787da8eff8f88
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-08 05:58:48 +0530

    Time: 2 ms (93.33%), Space: 68.4 MB (27.32%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int N = nums.length;
+        int goal = N - 1;
+
+        for(int i = N - 2; i >= 0; i--) {
+            if(nums[i] + i >= goal) 
+                goal = i;
+        }
+        
+        return goal == 0;
+    }
+}
