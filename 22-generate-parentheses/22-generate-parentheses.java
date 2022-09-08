48139a4b191804defdc752e3695734c496f2b813
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-08 07:43:31 +0530

    Time: 1 ms (97.71%), Space: 43.4 MB (69.92%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,22 +1,29 @@
 class Solution {
     private List<String> ans = new ArrayList<>();
+    private StringBuilder sb = new StringBuilder();
     
     public List<String> generateParenthesis(int N) {
-        this.backtrack("", 0, 0, N);
+        this.backtrack(0, 0, N);
         
         return this.ans;
     }
     
-    private void backtrack(String curr, int open, int close, int N) {
-        if(curr.length() == 2 * N) {
-            this.ans.add(curr);
+    private void backtrack(int open, int close, int N) {
+        if(sb.length() == 2 * N) {
+            this.ans.add(sb.toString());
             return;
         }
     
-        if(open < N)
-            this.backtrack(curr.concat("("), open + 1, close, N);
+        if(open < N) {
+            sb.append("(");
+            this.backtrack(open + 1, close, N);
+            sb.deleteCharAt(sb.length() - 1);
+        }
         
-        if(close < open)
-            this.backtrack(curr.concat(")"), open, close + 1, N);
+        if(close < open) {
+            sb.append(")");
+            this.backtrack(open, close + 1, N);
+            sb.deleteCharAt(sb.length() - 1);            
+        }
     }
 }
