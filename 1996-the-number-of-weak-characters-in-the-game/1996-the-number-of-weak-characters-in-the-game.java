8fc592102cc93a32b92a6a34d527a31fee42d327
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-09 06:43:01 +0530

    Time: 11 ms (91.72%), Space: 129.4 MB (76.61%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -12,18 +12,38 @@ class Solution {
     }
     
     public int numberOfWeakCharacters(int[][] properties) {
-        int[] max_defence = new int[100002];
-        int ans = 0;
+        int maxAttack = 0;
+        // Find the maximum atack value
+        for (int[] property : properties) {
+            int attack = property[0];
+            maxAttack = Math.max(maxAttack, attack);
+        }
 
-        for (int[] prop : properties)
-            max_defence[prop[0]] = Math.max(max_defence[prop[0]], prop[1]);
+        int maxDefense[] = new int[maxAttack + 2];
+        // Store the maximum defense for an attack value
+        for (int[] property : properties) {
+            int attack = property[0];
+            int defense = property[1];
 
-        for (int i = 100000; i > 0; --i)
-            max_defence[i - 1] = Math.max(max_defence[i - 1], max_defence[i]);
+            maxDefense[attack] = Math.max(maxDefense[attack], defense);
+        }
 
-        for (int[] prop : properties)
-            ans += max_defence[prop[0] + 1] > prop[1] ? 1 : 0;
+        // Store the maximum defense for attack greater than or equal to a value
+        for (int i = maxAttack - 1; i >= 0; i--) {
+            maxDefense[i] = Math.max(maxDefense[i], maxDefense[i + 1]);
+        }
 
-        return ans;
+        int weakCharacters = 0;
+        for (int[] property : properties) {
+            int attack = property[0];
+            int defense = property[1];
+
+            // If their is a greater defense for properties with greater attack
+            if (defense < maxDefense[attack + 1]) {
+                weakCharacters++;
+            }
+        }
+
+        return weakCharacters;
     }
 }
