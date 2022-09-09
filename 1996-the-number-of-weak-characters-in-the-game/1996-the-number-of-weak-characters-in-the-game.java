086cc0c69296853718d93d3692fcc02bdcef0954
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-09 06:39:05 +0530

    Time: 25 ms (88.75%), Space: 145.2 MB (20.17%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int numberOfWeakCharacters(int[][] properties) {
+    public int numberOfWeakCharacters_NlogN(int[][] properties) {
         int max_defence = 0, ans = 0;
         Arrays.sort(properties, (p1, p2) -> p1[0] == p2[0] ? p1[1] - p2[1] : p2[0] - p1[0]);
 
@@ -7,6 +7,22 @@ class Solution {
             ans += prop[1] < max_defence ? 1 : 0;
             max_defence = Math.max(max_defence, prop[1]);
         }
+
+        return ans;
+    }
+    
+    public int numberOfWeakCharacters(int[][] properties) {
+        int[] max_defence = new int[100002];
+        int ans = 0;
+
+        for (int[] prop : properties)
+            max_defence[prop[0]] = Math.max(max_defence[prop[0]], prop[1]);
+
+        for (int i = 100000; i > 0; --i)
+            max_defence[i - 1] = Math.max(max_defence[i - 1], max_defence[i]);
+
+        for (int[] prop : properties)
+            ans += max_defence[prop[0] + 1] > prop[1] ? 1 : 0;
 
         return ans;
     }
