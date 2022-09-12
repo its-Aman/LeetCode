45f00210b0f4ac91e66f42a71a4f35a336b80099
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-12 19:54:36 +0530

    Time: 73 ms (41.47%), Space: 87.8 MB (5.44%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,16 +1,42 @@
 class Solution {
+    Random random = new Random();
+    
     public int findKthLargest(int[] nums, int k) {
-        int N = (int)10e4;
-        int[] keep = new int[N * 2 + 2];
+        List<Integer> list = new ArrayList<>();
         
         for(int num: nums)
-            ++keep[num + N];
+            list.add(num);
         
-        int i = -1;
-        for(i = N * 2 + 1; i >= 0 && k > 0; i--)
-            k -= keep[i] == Integer.MAX_VALUE ? 0 : keep[i];
-        
-        return i - N + 1;
+        return findKthLargest(list, k);
     }
+    
+    public int findKthLargest(List<Integer> nums, int k) {
+        if(nums == null || nums.size() == 0)
+            return 0;
+        
+        int pivot = nums.get(random.nextInt(nums.size()));
+        List<Integer> left = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        
+        for(int num: nums) {
+            if(num > pivot)
+                left.add(num);
+            else if(num < pivot)
+                right.add(num);
+            else
+                mid.add(num);
+        }
+        
+        int L = left.size();
+        int M = mid.size();
+        
+        if(k <= L)
+            return findKthLargest(left, k);
+        else if(k > L + M)
+            return findKthLargest(right, k - L - M);
+        else
+            return mid.get(0);        
+    }
+
 }
-// 200002
