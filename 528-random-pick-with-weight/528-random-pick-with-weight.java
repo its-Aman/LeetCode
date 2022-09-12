22284dbadc24497a47dbb68b014573e0ef6f39af
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-12 18:04:09 +0530

    Time: 62 ms (26.70%), Space: 59.3 MB (16.60%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.java
+++ 528-random-pick-with-weight/528-random-pick-with-weight.java
@@ -1,35 +1,78 @@
 class Solution {
-    int[] prefix;
-    int N;
-    Random random;
+    final double EPSILON = 0.000001;
+    Random random = new Random();
+    List<Box[]> list = new ArrayList<>();
 
     public Solution(int[] w) {
-        N = w.length;
-        random = new Random();
+        if (w == null || w.length <= 0)
+            return;
 
-        for (int i = 1; i < N; i++)
-            w[i] += w[i - 1];
+        double sum = 0;
 
-        prefix = w;
-    }
-    
-    public int pickIndex() {
-        int num = random.nextInt(prefix[N - 1]) + 1;
-        int l = 0, r = N - 1;
+        for (int wt : w)
+            sum += wt;
 
-        while (l < r) {
-            int m = l + (r - l) / 2;
-            if (prefix[m] == num)
-                return m;
-            else if (num > prefix[m])
-                l = m + 1;
+        double avg = sum / w.length;
+
+        List<Box> largerList = new ArrayList<>();
+        List<Box> smallerList = new ArrayList<>();
+
+        for (int i = 0; i < w.length; i++) {
+            Box box = new Box(i, w[i]);
+
+            if (box.val >= avg - EPSILON)
+                largerList.add(box);
             else
-                r = m;
+                smallerList.add(box);
         }
 
-        return l;
+        while (smallerList.size() > 0) {
+            Box smaller = smallerList.remove(smallerList.size() - 1);
+            Box larger = largerList.get(largerList.size() - 1);
+
+            list.add(new Box[]{smaller, new Box(larger.i, avg - smaller.val)});
+
+            larger.val -= avg - smaller.val;
+
+            if (larger.val < avg - EPSILON) {
+                largerList.remove(largerList.size() - 1);
+                if (larger.val > EPSILON)
+                    smallerList.add(larger);
+            }
+        }
+
+        for (Box box : largerList)
+            list.add(new Box[]{box, null});
+
     }
 
+    public int pickIndex() {
+        if (list.size() == 0)
+            return -1;
+
+        int idx = random.nextInt(list.size());
+        Box[] pair = list.get(idx);
+
+        if (pair[1] == null)
+            return pair[0].i;
+
+        double randomData = random.nextDouble(pair[0].val + pair[1].val);
+
+        if (randomData < pair[0].val)
+            return pair[0].i;
+
+        return pair[1].i;
+    }
+
+    class Box {
+        int i;
+        double val;
+
+        public Box(int i, double val) {
+            this.i = i;
+            this.val = val;
+        }
+    }
 }
 
 /**
