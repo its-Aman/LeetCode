74c478d62c1bbd06c44d22dc3ac638f955c6b339
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-13 07:36:59 +0530

    Time: 2 ms (79.27%), Space: 46.8 MB (84.46%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean validUtf8(int[] data) {
         int numberOfByteToProcess = 0;
+        int mask1 = 1 << 7;
+        int mask2 = 1 << 6;
 
         for (int i = 0; i < data.length; i++) {
-            String curr_utf_8 = getByte(data[i]);
-
             if (numberOfByteToProcess == 0) {
-                while (numberOfByteToProcess < curr_utf_8.length() && curr_utf_8.charAt(numberOfByteToProcess) != '0')
+                int mask = 1 << 7;
+
+                while ((mask & data[i]) != 0) {
                     ++numberOfByteToProcess;
+                    mask >>= 1;
+                }
 
                 if (numberOfByteToProcess == 0)
                     continue;
@@ -16,23 +20,12 @@ class Solution {
                     return false;
 
             } else {
-                if (curr_utf_8.charAt(0) != '1' || curr_utf_8.charAt(1) != '0')
+                if ((mask1 & data[i]) == 0 || (mask2 & data[i]) != 0)
                     return false;
             }
             --numberOfByteToProcess;
         }
 
         return numberOfByteToProcess == 0;
-    }
-
-    private String getByte(int num) {
-        String bits = Integer.toBinaryString(num);
-
-        if (bits.length() >= 8)
-            bits = bits.substring(bits.length() - 8);
-        else
-            bits = "0".repeat(8 - bits.length()).concat(bits);
-
-        return bits;
     }
 }
