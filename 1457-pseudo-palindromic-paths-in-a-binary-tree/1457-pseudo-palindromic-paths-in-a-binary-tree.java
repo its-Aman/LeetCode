15d2148bc61b89875d837ab4785d0b4533699994
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-14 06:12:17 +0530

    Time: 697 ms (5.26%), Space: 127.2 MB (21.05%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -17,61 +17,25 @@ class Solution {
     private int ans = 0;
 
     public int pseudoPalindromicPaths (TreeNode root) {
-        int[] count = new int[10];
-        ++count[root.val];
-        
-        findPath(root, count, 1);
-        
+        findPath(root, 0);
         return ans;
     }
     
-    private void findPath(TreeNode node, int[] count, int size) {
+    private void findPath(TreeNode node, int mask) {
+        if(node == null)
+            return;
+        
+        mask ^= (1 << node.val);
+
+        
         if(node.left == null && node.right == null) {
-            checkIfPseudoPalindrome(count, size);
+            System.out.println(mask);
+            if(mask == 0 || (mask & (mask - 1)) == 0)
+                ++ans;
             return;
         }
         
-        if(node.left != null) {
-            int[] lCount = count.clone();        
-            ++lCount[node.left.val];
-            findPath(node.left, lCount, size + 1);
-        }
-
-        if(node.right != null) {
-            int[] rCount = count.clone();        
-            ++rCount[node.right.val];
-            findPath(node.right, rCount, size + 1);
-        }       
-    }
-    
-    private void checkIfPseudoPalindrome(int[] cnt, int size) {
-        boolean isPali = true;
-        
-        if(size % 2 == 0) {
-            // check for even palindrome
-            for(int occ: cnt) {
-                if(occ % 2 == 1) {
-                    isPali = false;
-                    break;
-                }    
-            }
-        } else {
-            // check for odd palindrome
-            boolean allowOne = true;
-            
-            for(int occ: cnt) {
-                if(occ % 2 == 1) {
-                    if(allowOne) {
-                        allowOne = false;
-                    } else {                        
-                        isPali = false;
-                        break;
-                    }
-                }    
-            }
-        }
-        
-        if(isPali)
-            ++ans;
+        findPath(node.left, mask);
+        findPath(node.right, mask);
     }
 }
