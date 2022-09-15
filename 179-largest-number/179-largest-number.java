7b7e0e508624adf8c63014c495e5e0a38108a400
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-15 21:59:04 +0530

    Time: 16 ms (16.90%), Space: 43.4 MB (72.47%) - LeetHub

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -5,8 +5,8 @@ class Solution {
         for (int i = 0; i < nums.length; i++)
             strArr[i] = Integer.toString(nums[i]);
 
-        // Arrays.sort(strArr, (a, b) -> (b + a).compareTo((a + b)));
-        Arrays.sort(strArr, (a, b) -> b.concat(a).compareTo(a.concat(b)));
+        Arrays.sort(strArr, (a, b) -> (b + a).compareTo((a + b)));
+        // Arrays.sort(strArr, (a, b) -> b.concat(a).compareTo(a.concat(b)));
         String ans = String.join("", strArr);
 
         if (ans.charAt(0) == '0')
