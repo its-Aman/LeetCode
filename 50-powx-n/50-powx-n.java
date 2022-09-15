7aacd8bde11d33645915c10b7142729992aec3b8
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-15 07:48:38 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (91.35%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,24 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
         double ans = 1;
+        long ln = (long)n;
         
         if(n == 0)
             return ans;
 
-        if(n == Integer.MIN_VALUE)
-            return 1/x * myPow(x, -Integer.MAX_VALUE);
+        // if(n == Integer.MIN_VALUE)
+        //     return 1/x * myPow(x, -Integer.MAX_VALUE);
         
-        if(n < 0) {
+        if(ln < 0) {
             x = 1/x;
-            n = -n;
+            ln = -ln;
         }
         
-        while(n > 0) {
-            if((n & 1) == 1) 
+        while(ln > 0) {
+            if((ln & 1) == 1) 
                 ans *= x;
 
             x *= x;
-            n >>= 1;                
+            ln >>= 1;                
         }
         
         return ans;
