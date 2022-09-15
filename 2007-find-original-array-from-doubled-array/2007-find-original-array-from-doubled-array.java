8dc2bec36645a0a6083ab9baca9c2ce06940fcf7
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-15 06:53:31 +0530

    Time: 14 ms (95.51%), Space: 123.4 MB (86.23%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,5 +1,46 @@
 class Solution {
+    
     public int[] findOriginalArray(int[] changed) {
+        int N = changed.length;
+
+        if (N % 2 == 1)
+            return new int[0];
+
+        int max = Integer.MIN_VALUE;
+
+        for (int num : changed)
+            max = Math.max(max, num);
+
+        if (max % 2 == 1)
+            return new int[0];
+
+        int[] counter = new int[max + 1];
+        int[] ans = new int[N / 2];
+
+        for (int num : changed)
+            ++counter[num];
+
+        if (counter[0] % 2 == 1)
+            return new int[0];
+
+        int idx = counter[0] / 2;
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] == 0)
+                continue;
+
+            if (i + i > max || counter[i] > counter[i + i])
+                return new int[0];
+
+            counter[i + i] -= counter[i];
+            while (counter[i]-- > 0)
+                ans[idx++] = i;
+        }
+
+        return ans;
+    }
+
+    public int[] findOriginalArray_2(int[] changed) {
         int N = changed.length;
 
         if (N % 2 == 1)
