09aae81be8b23bc8f1f1542a825655b5ebb5ea78
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-17 13:27:39 +0530

    Time: 248 ms (98.02%), Space: 111.3 MB (80.76%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.java
+++ 336-palindrome-pairs/336-palindrome-pairs.java
@@ -1,17 +1,5 @@
 class Solution {
 
-    class PalindromePair {
-        String palindrome;
-        int x;
-        int y;
-
-        PalindromePair(String palindrome, int x, int y) {
-            this.palindrome = palindrome;
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     public List<List<Integer>> palindromePairs(String[] words) {
         HashMap<String, Integer> wordMap = new HashMap<>();
         Set<Integer> set = new TreeSet<>();
@@ -37,20 +25,24 @@ class Solution {
             sb.append(words[i]);
             sb.reverse();
             String reverse = sb.toString();
-            if (wordMap.containsKey(reverse) && wordMap.get(reverse) != i) {
+
+            if (wordMap.containsKey(reverse) && wordMap.get(reverse) != i)
                 result.add(Arrays.asList(i, wordMap.get(reverse)));
-            }
+
             for (Integer k : set) {
-                if (k == length) break;
+                if (k == length)
+                    break;
 
                 if (isPalindrome(reverse, 0, length - 1 - k)) {
                     String s1 = reverse.substring(length - k);
-                    if (wordMap.containsKey(s1)) result.add(Arrays.asList(i, wordMap.get(s1)));
+                    if (wordMap.containsKey(s1))
+                        result.add(Arrays.asList(i, wordMap.get(s1)));
                 }
 
                 if (isPalindrome(reverse, k, length - 1)) {
                     String s2 = reverse.substring(0, k);
-                    if (wordMap.containsKey(s2)) result.add(Arrays.asList(wordMap.get(s2), i));
+                    if (wordMap.containsKey(s2))
+                        result.add(Arrays.asList(wordMap.get(s2), i));
                 }
             }
         }
@@ -58,9 +50,9 @@ class Solution {
         return result;
     }
 
-    private boolean isPalindrome(String s, int left, int right) {
+    private boolean isPalindrome(String word, int left, int right) {
         while (left < right)
-            if (s.charAt(left++) != s.charAt(right--))
+            if (word.charAt(left++) != word.charAt(right--))
                 return false;
 
         return true;
