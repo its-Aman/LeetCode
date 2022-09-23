137dfa11d97e6e8a4e9205128141d9f6dcba64ea
Aman Kumar <kumaraman7056@hotmail.com>
2022-09-23 07:49:38 +0530

    Time: 80 ms (100.00%), Space: 39 MB (97.40%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -2,14 +2,15 @@ class Solution {
     public int concatenatedBinary(int n) {
         long ans = 0;
         int M = 1_000_000_007;
+        int bitCnt = 0;
 
-        for (int i = 1; i <= n; i++)
-            ans = ((ans << log2(i)) | i) % M;
+        for (int i = 1; i <= n; i++) {
+            if((i & (i - 1)) == 0)
+                ++bitCnt;
+
+            ans = ((ans << bitCnt) | i) % M;
+        }
 
         return (int) (ans % M);
-    }
-
-    private int log2(long x) {
-        return 1 + (int) (Math.log(x) / Math.log(2));
     }
 }
