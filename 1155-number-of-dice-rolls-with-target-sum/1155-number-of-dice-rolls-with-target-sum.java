74da05cd115689f47d1a5fd160c8ccea58fd885b
Aman Kumar <kumaraman7056@hotmail.com>
2022-10-03 06:48:12 +0530

    Time: 8 ms (98.53%), Space: 41 MB (94.24%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,24 +1,33 @@
 class Solution {
-    int[][] dp = new int[31][1001];
     
-    public int numRollsToTarget2(int n, int k, int target) {
+    public int numRollsToTarget(int dice, int face, int target) {
         int MOD = 1000000007;
+        int[] dp = new int[target + 1];
         
-        if(n == 0 || target <= 0)
-            return n == target ? 1 : 0;
+        if(dice == 0 || target <= 0)
+            return dice == target ? 1 : 0;
+        
+        dp[0] = 1;
+            
+        for(int d = 1; d <= dice; d++) {
+            int[] next = new int[target + 1];
+            int prev = dp[0];
+            
+            for(int t = 1;  t <= target; t++) {
+                next[t] = prev;
+                prev = (prev + dp[t]) % MOD;
+                
+                if(t - face >= 0)
+                    prev = (prev - dp[t - face] + MOD) % MOD;
+            }
+            
+            dp = next;
+        }
 
-        if(dp[n][target] != 0)
-            return dp[n][target] - 1;
-        
-        int ans = 0;
-        for(int i = 1; i <= k; i++)
-            ans = (ans + numRollsToTarget(n - 1, k, target - i)) % MOD;
-        
-        dp[n][target] = ans + 1;
-        return ans;
+        return dp[target];
     }
     
-    public int numRollsToTarget(int n, int k, int target) {
+    public int numRollsToTarget2(int n, int k, int target) {
         int MOD = 1000000007;
         int[] dp = new int[target + 1];
         
