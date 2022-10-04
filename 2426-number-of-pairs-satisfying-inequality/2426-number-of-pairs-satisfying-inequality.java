ffce28c9526e56bcef6c5c870764e9c35f8239b3
Aman Kumar <kumaraman7056@hotmail.com>
2022-10-04 21:43:47 +0530

    Time: 25 ms (93.98%), Space: 90.7 MB (46.07%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.java
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.java
@@ -1,44 +1,38 @@
 class Solution {
-    long ans = 0;
-
-    public long numberOfPairs(int[] nums1, int[] nums2, int k) {
-//        nums1[i] - nums1[j] <= nums2[i] - nums2[j] + diff
-//        nums1[i] - nums2[i] <= nums1[j] - nums2[j] + diff
-//        nums[i] <= nums[j] + diff
-
-        int N = nums2.length;
-        int[] diff = new int[N];
-
-        for (int i = 0; i < N; i++)
-            diff[i] = nums1[i] - nums2[i];
-
-        mergeSort(diff, 0, N - 1, k);
-        return ans;
-    }
-
-    private void mergeSort(int[] diff, int l, int r, int k) {
-        if (l == r)
-            return;
-
-        int m = l + (r - l) / 2;
-        mergeSort(diff, l, m, k);
-        mergeSort(diff, m + 1, r, k);
-
-        merge(diff, l, m, r, k);
-    }
-
-    private void merge(int[] diff, int l, int m, int r, int k) {
-        int start = l, end = m + 1;
-
-        while (start <= m && end <= r) {
-            if (diff[start] <= diff[end] + k) {
-                ans += r - end + 1;
-                start++;
-            } else {
-                end++;
-            }
+    int[] bit;
+    
+    public long numberOfPairs(int[] nums1, int[] nums2, int diff) {
+        bit = new int [60_001];    
+        
+        long result = 0;
+        int d;
+        
+        for (int i = 0; i < nums1.length; ++i){
+            d = nums1[i] - nums2[i];
+            
+            result += getBitVal(d + diff);
+            addToBit(d);
         }
-
-        Arrays.sort(diff, l, r + 1);
+        
+        return result;
+    }
+    
+    private void addToBit(int val){
+        val += 30_000;
+        
+        if (val <= 0) return;
+        
+        for (int i = val; i < bit.length; i += (i & -i))
+            bit[i]++;
+    }
+    
+    private int getBitVal(int val){
+        val += 30_000;
+        int result = 0;
+        
+        for (int i = val; i > 0; i -= (i & -i))
+            result += bit[i];    
+        
+        return result;
     }
 }
