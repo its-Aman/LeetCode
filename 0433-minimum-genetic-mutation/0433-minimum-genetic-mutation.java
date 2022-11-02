7c91073ccc216cd23c4b1666185507e70d4aa2a5
Aman Kumar <kumaraman7056@hotmail.com>
2022-11-02 06:30:56 +0530

    Time: 1 ms (92.51%), Space: 40.3 MB (89.30%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -1,42 +1,44 @@
 class Solution {
-    Set<String> set = new HashSet<>();
-    char[] genes = new char[]{'A', 'C', 'G', 'T'};
-    int ans = Integer.MAX_VALUE;
-
     public int minMutation(String start, String end, String[] bank) {
+        Queue<String> q = new LinkedList<>();
+        Set<String> seen = new HashSet<>();
+        Set<String> set = new HashSet<>();
+        char[] genes = new char[]{'A', 'C', 'G', 'T'};
+
         for (String b : bank)
             set.add(b);
 
-        set.add(start);
+        q.add(start);
+        seen.add(start);
 
-        if (!set.contains(end))
-            return -1;
+        int steps = 0;
 
-        bfs(start.toCharArray(), end.toCharArray(), 0);
+        while (!q.isEmpty()) {
+            int size = q.size();
 
-        if (ans == Integer.MAX_VALUE)
-            return -1;
+            while (size-- > 0) {
+                char[] node = q.poll().toCharArray();
 
-        return ans;
-    }
+                if (String.valueOf(node).equals(end))
+                    return steps;
 
-    private void bfs(char[] start, char[] end, int cnt) {
-        if (Arrays.equals(start, end))
-            ans = Math.min(ans, cnt);
-
-        for (int i = 0; i < 8; i++) {
-            char temp = start[i];
-
-            for (char g : genes) {
-                start[i] = g;
-                String str = String.valueOf(start);
-                if (set.contains(str)) {
-                    set.remove(str);
-                    bfs(start, end, cnt + 1);
-                    set.add(str);
+                for (char c : genes) {
+                    for (int i = 0; i < 8; i++) {
+                        char temp = node[i];
+                        node[i] = c;
+                        String nbr = String.valueOf(node);
+                        if (!seen.contains(nbr) && set.contains(nbr)) {
+                            q.add(nbr);
+                            seen.add(nbr);
+                        }
+                        node[i] = temp;
+                    }
                 }
             }
-            start[i] = temp;
+            
+            steps++;
         }
+        
+        return -1;
     }
 }
