b0dce9bf5e766f16c9468133abfc013b0c362c41
Aman Kumar <kumaraman7056@hotmail.com>
2022-11-22 06:31:37 +0530

    Time: 1 ms (100.00%), Space: 39.6 MB (99.27%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,7 +1,29 @@
-// https://www.youtube.com/watch?v=aZuQXkO0-XA
-
 class Solution {
+    
     public int numSquares(int n) {
+        return numSquares_math(n);
+    }
+    
+    // https://www.youtube.com/watch?v=1xfx6M_GqFk
+    public int numSquares_math(int n) {
+        if(isSquare(n))
+            return 1;
+        
+        while((n & 3) == 0) // n%4
+            n >>= 2;
+
+        if((n & 7) == 7)  // n%8
+           return 4;
+           
+       for(int i = 1; i*i <= n; i++)
+           if(isSquare(n - i*i))
+               return 2;
+           
+       return 3;
+    }
+    
+    // https://www.youtube.com/watch?v=aZuQXkO0-XA
+    public int numSquares_dp(int n) {
         int ans = 0;
         int[] dp = new int[n + 1];
         
@@ -13,5 +35,10 @@ class Solution {
         }
         
         return dp[n];
+    }
+    
+    private boolean isSquare(int n) {
+        double x = Math.sqrt(n);
+        return Math.ceil(x) == Math.floor(x);
     }
 }
