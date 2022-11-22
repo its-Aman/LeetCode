52a0a98a6b5c4bc3e609c4bf10d1b3d30abe7609
Aman Kumar <kumaraman7056@hotmail.com>
2022-11-22 06:06:14 +0530

    Time: 46 ms (70.67%), Space: 42.9 MB (69.53%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSquares(int n) {
+        int ans = 0;
+        int[] dp = new int[n + 1];
+        
+        for(int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            
+            for(int j = 1; j * j <= i; j++)
+                dp[i] = Math.min(dp[i], 1 + dp[i - j*j]);
+        }
+        
+        return dp[n];
+    }
+}
