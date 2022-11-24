6ab5812446ddf3ff654ff450f9f102b1997f236d
Aman Kumar <kumaraman7056@hotmail.com>
2022-11-24 06:53:51 +0530

    Time: 176 ms (60.89%), Space: 42 MB (68.68%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,43 @@
+class Solution {
+    int[][] dirs = new int[][] {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    int R, C;
+    
+    public boolean exist(char[][] board, String word) {
+        R = board.length;
+        C = board[0].length;
+        
+        for(int r = 0; r < R; r++) {
+            for(int c = 0; c < C; c++) {
+                if(board[r][c] == word.charAt(0)) {
+                    char temp = board[r][c];
+                    board[r][c] = '.';
+                    if(isPresent(board, word, r, c, 1))
+                        return true;
+                    board[r][c] = temp;
+                }
+            }
+        }
+        
+        return false;
+    }
+        
+    private boolean isPresent(char[][] board, String word, int r, int c, int idx) {
+        if(idx == word.length())
+            return true;
+        
+        for(int[] dir: dirs) {
+            int nr = r + dir[0],
+                nc = c + dir[1];
+            
+            if(nr >= 0 && nr < R && nc >= 0 && nc < C && board[nr][nc] == word.charAt(idx)) {
+                char temp = board[nr][nc];
+                board[nr][nc] = '.';
+                if(isPresent(board, word, nr, nc, idx + 1))
+                    return true;
+                board[nr][nc] = temp;
+            }
+        }
+        
+        return false;
+    }
+}
