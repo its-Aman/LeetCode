49956b1b92fa4ff4152a5d7f8bf5e437ef6f7d2e
Aman Kumar <kumaraman7056@hotmail.com>
2022-11-25 07:04:06 +0530

    Time: 86 ms (68.33%), Space: 50.3 MB (81.92%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        return sumSubarrayMins_stackAndDP(arr);
+    }
+
+    public int sumSubarrayMins_stackAndDP(int[] arr) {
+        int MOD = 1000000007;
+        int N = arr.length;
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        int[] dp = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i])
+                stack.pop();
+            
+            if(stack.isEmpty())
+                dp[i] = (i + 1) * arr[i];
+            else 
+                dp[i] = dp[stack.peek()] + (i - stack.peek()) * arr[i];
+            
+            stack.push(i);
+        }
+        
+        for(int cnt : dp)
+            ans = (ans + cnt) % MOD;
+        
+        return ans;
+    }
+    
+    public int sumSubarrayMins_stackOnly(int[] arr) {
+        int MOD = 1000000007;
+        int N = arr.length;
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i <= N; i++) {
+            while(!stack.isEmpty() && (i == N || arr[stack.peek()] >= arr[i])) {
+                int mid = stack.pop();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+
+                ans = (ans + arr[mid] * (i - mid) * (mid - left)) % MOD;
+            }
+            
+            stack.push(i);
+        }
+        
+        return ans;
+    }
+}
