56a0764fda7fef8d7df3b685071f2bbbf2038d97
Aman Kumar <kumaraman7056@hotmail.com>
2022-11-26 07:57:29 +0530

    Time: 7 ms (100.00%), Space: 69.1 MB (77.99%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,5 +1,56 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        return jobScheduling_dp(startTime, endTime, profit);
+    }
+    
+    public int jobScheduling_dp(int[] startTime, int[] endTime, int[] profit) {
+        int N = profit.length;
+        
+        // bubble sort
+        for(int i = 0; i < N; i++) {
+            int s = startTime[i],
+                e = endTime[i],
+                p = profit[i];
+            int j = i - 1;
+            
+            while(j >= 0 && endTime[j] > e) {
+                startTime[j + 1] = startTime[j];
+                endTime[j + 1] = endTime[j];
+                profit[j + 1] = profit[j];
+                j--;
+            }
+
+            startTime[j + 1] = s;
+            endTime[j + 1] = e;
+            profit[j + 1] = p;
+        }
+        
+        int[] max = new int[N];
+        max[0] = profit[0];
+        
+        for(int i = 1; i < N; i++) {
+            int prev = i - 1;
+            int s = startTime[i],
+                p = profit[i];
+            
+            if(endTime[prev] > s) {
+                while(prev >= 0 && endTime[prev] > s)
+                    prev--;
+                
+                // perform knapsack/dp/greedy
+                if(prev >= 0)
+                    max[i] = Math.max(max[i-1], p + max[prev]);
+                else
+                    max[i] = Math.max(max[i-1], p);
+            } else {
+                max[i] = max[prev] + p;
+            }
+        }
+        
+        return max[N-1];
+    }
+    
+    public int jobScheduling_TreeMap(int[] startTime, int[] endTime, int[] profit) {
         int N = profit.length;
         int[][] jobs = new int[N][3];
 
@@ -18,4 +69,5 @@ class Solution {
 
         return dp.lastEntry().getValue();
     }
+
 }
