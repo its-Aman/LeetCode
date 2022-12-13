54e108298e33ed0f062d4ac580a1b1d24e71a306
Aman Kumar <kumaraman7056@hotmail.com>
2022-12-13 07:31:15 +0530

    Time: 1 ms (99.96%), Space: 42.7 MB (94.44%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,5 +1,9 @@
 class Solution {
     public int minFallingPathSum(int[][] M) {
+        return minFallingPathSum_memo(M);
+    }
+
+    public int minFallingPathSum_rec(int[][] M) {
         int ans = Integer.MAX_VALUE,
                 N = M.length;
         var dp = M.clone();
@@ -16,5 +20,35 @@ class Solution {
             ans = Math.min(ans, M[N-1][c]);
 
         return ans;
+    }
+    
+    public int minFallingPathSum_memo(int[][] M) {
+        int ans = Integer.MAX_VALUE,
+                N = M.length;
+        var dp = new int[N][N];
+
+        for(int c = 0; c < N; c++)
+            ans = Math.min(ans, dfs(0, c, M, dp));
+        
+        return ans;
+    }
+
+    private int dfs(int r, int c, int[][] M, int[][] dp) {
+        int N = M.length;
+        
+        if(r == N - 1 && c >= 0 && c <= N - 1)
+            return M[r][c];
+        
+        if(r >= N || c < 0 || c >= N)
+            return Integer.MAX_VALUE;
+        
+        if(dp[r][c] != 0)
+            return dp[r][c];
+        
+        int one = dfs(r + 1, c - 1, M, dp);
+        int two = dfs(r + 1, c, M, dp);
+        int three = dfs(r + 1, c + 1, M, dp);
+        
+        return dp[r][c] = M[r][c] + Math.min(one, Math.min(two, three));
     }
 }
