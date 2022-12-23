c04431318aec40881cfe304ca1ae7b007e8ea8b3
Aman Kumar <kumaraman7056@hotmail.com>
2022-12-23 07:20:18 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (92.35%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sell = 0,
+            prev_sell = 0,
+            buy = Integer.MIN_VALUE,
+            prev_buy = 0;
+        
+        for(int price: prices) {
+            prev_buy = buy;
+            buy = Math.max(prev_sell - price, prev_buy);
+            prev_sell = sell;
+            sell = Math.max(prev_buy + price, prev_sell);
+        }
+        
+        return sell;
+    }
+}
