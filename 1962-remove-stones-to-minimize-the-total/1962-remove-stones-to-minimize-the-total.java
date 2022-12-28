9a36660272c816ede6d08a485259ae9400caccdd
Aman Kumar <kumaraman7056@hotmail.com>
2022-12-28 07:48:09 +0530

    Time: 4 ms (100.00%), Space: 55.1 MB (96.74%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -1,19 +1,42 @@
-class Solution {
-    public int minStoneSum(int[] piles, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        int ans = 0;
+// class Solution {
+//     public int minStoneSum(int[] piles, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+//         int ans = 0;
 
-        for(int pile: piles) {
-            ans += pile;
-            pq.offer(pile);
+//         for(int pile: piles) {
+//             ans += pile;
+//             pq.offer(pile);
+//         }
+        
+//         while(k-- > 0 && !pq.isEmpty()) {
+//             int item = pq.poll();
+//             pq.offer(item - item / 2);
+//             ans -= item / 2;
+//         }
+        
+//         return ans;
+//     }
+// }
+
+public class Solution {
+    public int minStoneSum(int[] piles, int k) {
+        int sum = 0;
+        int a[] = new int[10001];
+        
+        for (int i : piles) {
+            sum += i;
+            a[i]++;
         }
         
-        while(k-- > 0 && !pq.isEmpty()) {
-            int item = pq.poll();
-            pq.offer(item - item / 2);
-            ans -= item / 2;
+        for (int i = 10000; i > 0 && k > 0; i--) {
+            if (a[i] > 0) {
+                int b = i / 2;
+                sum -= Math.min(k, a[i]) * b;
+                a[i - i / 2] += a[i];
+                k -= a[i];
+            }
         }
-        
-        return ans;
+        return sum;
+
     }
 }
