1903b603efae4fd909c7b9bbad9f65b1bd115423
Aman Kumar <kumaraman7056@hotmail.com>
2023-01-25 07:31:46 +0530

    Time: 10 ms (97.60%), Space: 58.7 MB (84.43%)
    - Runtime: 10 ms, faster than 97.60% of Java online submissions for Find Closest Node to Given Two Nodes.
    - Memory Usage: 58.7 MB, less than 84.43% of Java online submissions for Find Closest Node to Given Two Nodes.

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,44 +1,38 @@
 class Solution {
-    public int closestMeetingNode(int[] edges, int node1, int node2) {
-      int dist = 0;
-      int curr = 0;
-      int node1Dist[] = new int[edges.length];
-      int node2Dist[] = new int[edges.length];
-      
-      Arrays.fill(node1Dist, -1);
-      Arrays.fill(node2Dist, -1);
-      
-      curr = node1;
-      dist = 0;
-      
-      while(curr != -1 && node1Dist[curr] == -1) {
-        node1Dist[curr] = dist;
-        curr = edges[curr];
-        dist += 1;
-      }      
-      
-      curr = node2;
-      dist = 0;
-      
-      while(curr != -1 && node2Dist[curr] == -1) {
-        node2Dist[curr] = dist;
-        curr = edges[curr];
-        dist += 1;
-      }      
+    public int closestMeetingNode(int[] edges, int n1, int n2) {
+        int N = edges.length,
+            ans = -1, 
+            max = Integer.MAX_VALUE;
 
-      int sol = -1;
-      int best = Integer.MAX_VALUE;
-      
-      for(int i = 0; i < edges.length; i++) {
-        if(node1Dist[i] == -1 || node2Dist[i] == -1)
-          continue;
+        int[] d1 = calcDepth(edges, n1);
+        int[] d2 = calcDepth(edges, n2);
         
-        if(Math.max(node1Dist[i], node2Dist[i]) < best) {
-          best = Math.max(node1Dist[i], node2Dist[i]);
-          sol = i;
+        for(int i = 0; i < N; i++) {
+            if(d1[i] == -1 || d2[i] == -1) {
+                continue;
+            }
+            
+            if(max > Math.max(d1[i], d2[i])) {
+                max = Math.max(d1[i], d2[i]);
+                ans = i;
+            }
         }
-      }
-      
-      return sol;
+        
+        return ans;
+    }
+    
+    private int[] calcDepth(int[] edges, int node) {
+        int[] dist = new int[edges.length];
+        int curr = node, depth = 0;
+
+        Arrays.fill(dist, -1);
+        
+        while(curr != -1 && dist[curr] == -1) {
+            dist[curr] = depth;
+            curr = edges[curr];
+            depth += 1;
+        }
+        
+        return dist;
     }
 }
