5e5c6158a5ef773a7433ef98da8369b54da9a929
Aman Kumar <kumaraman7056@hotmail.com>
2023-01-28 08:07:45 +0530

    Time: 100 ms (44.54%), Space: 76.1 MB (5.88%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
@@ -1,8 +1,51 @@
 class SummaryRanges {
+    TreeMap<Integer, Integer> treeMap;
+
+    public SummaryRanges() {
+        treeMap = new TreeMap<>();
+    }
+
+    public void addNum(int value) {
+        int left = value, right = value;
+
+        var leftLeft = treeMap.floorEntry(value);
+        if (leftLeft != null) {
+            if (leftLeft.getValue() >= value) {
+                return;
+            }
+
+            if (leftLeft.getValue() == value - 1) {
+                left = leftLeft.getKey();
+            }
+        }
+
+        var rightRight = treeMap.higherEntry(value);
+        if (rightRight != null && rightRight.getKey() == value + 1) {
+            treeMap.remove(right + 1);
+            right = rightRight.getValue();
+        }
+
+        treeMap.put(left, right);
+    }
+
+    public int[][] getIntervals() {
+        int[][] ans = new int[treeMap.size()][2];
+        int i = 0;
+
+        for (var e : treeMap.entrySet()) {
+            ans[i++] = new int[]{e.getKey(), e.getValue()};
+        }
+
+        return ans;
+    }
+}
+
+
+class SummaryRanges2 {
     boolean[] map;
     int min, max;
 
-    public SummaryRanges() {
+    public SummaryRanges2() {
         map = new boolean[10_002];
         min = Integer.MAX_VALUE;
         max = Integer.MIN_VALUE;
