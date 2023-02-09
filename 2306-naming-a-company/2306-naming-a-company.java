c80294664591e75a32f181017c5983b7b50c006e
Aman Kumar <kumaraman7056@hotmail.com>
2023-02-09 07:13:22 +0530

    Time: 135 ms (98.41%), Space: 59 MB (36.51%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.java
+++ 2306-naming-a-company/2306-naming-a-company.java
@@ -1,6 +1,38 @@
 class Solution {
     public long distinctNames(String[] ideas) {
         Set<String>[] map = new Set[26];
+        int[][] cnt = new int[26][26];
+        long ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+            map[i] = new HashSet<>();
+        }
+
+        for (String idea : ideas) {
+            map[idea.charAt(0) - 'a'].add(idea.substring(1));
+        }
+
+        for (int i = 0; i < 26; i++) {
+            for (String s : map[i]) {
+                for (int j = i + 1; j < 26; j++) {
+                    if (map[j].contains(s)) {
+                        cnt[i][j] += 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = i + 1; j < 26; j++) {
+                ans += 2L * (map[i].size() - cnt[i][j]) * (map[j].size() - cnt[i][j]);
+            }
+        }
+
+        return ans;
+    }
+
+    public long distinctNames2(String[] ideas) {
+        Set<String>[] map = new Set[26];
         long ans = 0;
 
         for (String idea : ideas) {
