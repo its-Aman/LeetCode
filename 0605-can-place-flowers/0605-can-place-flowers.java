491f55e07326a33c8a3480690c2bb52717d5a9cb
Aman Kumar <kumaraman7056@hotmail.com>
2023-03-20 06:14:17 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (25.94%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int N = flowerbed.length;
+                
+        for(int i = 0; i < N; i++) {
+            int pre = i - 1 < 0 ? 0 : flowerbed[i - 1];
+            int post = i + 1 >= N ? 0 : flowerbed[i + 1];
+            
+            if((pre | flowerbed[i] | post) == 0) {
+                n--;
+                flowerbed[i] = 1;
+            }
+        }
+        
+        return n <= 0;
+    }
+}
