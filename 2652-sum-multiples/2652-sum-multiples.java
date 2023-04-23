384d85511f51e49191d3fbd3322eee4dd7897617
Aman Kumar <kumaraman7056@hotmail.com>
2023-04-23 09:48:38 +0530

    Time: 1 ms (100.00%), Space: 40.5 MB (11.11%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -1,5 +1,15 @@
 class Solution {
     public int sumOfMultiples(int n) {
+        return divisible(n,3)+divisible(n,5)+divisible(n,7)-
+            divisible(n,15)-divisible(n,21)-divisible(n,35)+divisible(n,105);
+    }
+    
+    private int divisible(int n, int d) {
+        int cnt = n/d;
+        return d*cnt*(cnt+1)/2;
+    }
+    
+    public int sumOfMultiples2(int n) {
         int ans = 0;
 
         while (n > 0) {
