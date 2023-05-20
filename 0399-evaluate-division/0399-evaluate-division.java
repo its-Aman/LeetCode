074a3d9d17d08e0369994452ae2f1373adc112fe
Aman Kumar <kumaraman7056@hotmail.com>
2023-05-20 11:13:02 +0530

    Time: 1 ms (90.67%), Space: 41.4 MB (44.00%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,54 +1,45 @@
 class Solution {
-	Map<String, String> root = new HashMap<>();
-	Map<String, Double> dist = new HashMap<>();
-
-	private String find(String s) {
-		if (!this.root.containsKey(s)) {
-			this.root.put(s, s);
-			this.dist.put(s, 1.0);
-			return s;
-		}
-
-		if (this.root.get(s).equals(s)) {
-			return s;
-		}
-
-//		Path Compression
-		String lastParent = this.root.get(s), newParent = this.find(lastParent);
-
-		if (!lastParent.equals(newParent)) {
-			this.root.put(s, newParent);
-			this.dist.put(s, this.dist.get(s) * this.dist.get(lastParent));
-		}
-
-		return newParent;
-	}
-
-	public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-		int N = equations.size();
-		int M = queries.size();
-		double[] ans = new double[M];
-
-		for (int i = 0; i < N; i++) {
-			String x = equations.get(i).get(0), y = equations.get(i).get(1);
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        var g = buildGraph(equations, values);
+        double[] ans = new double[queries.size()];
+        Set<String> set = new HashSet<>();
+        
+        for(int i=0; i<queries.size(); i++) {
+            set.clear();
+            ans[i] = getPathWeight(queries.get(i).get(0), queries.get(i).get(1), set, g);
+        }
+        
+        return ans;
+    }
+    
+    private double getPathWeight(String start, String end, Set<String> visited, Map<String, Map<String, Double>> graph) {
+        if(!graph.containsKey(start)) return -1.0;
+        
+        if(graph.get(start).containsKey(end)) return graph.get(start).get(end);
+        
+        visited.add(start);
+        
+        for(var nbrKey: graph.get(start).keySet()) {
+            if(visited.contains(nbrKey)) continue;
             
-			String xx = this.find(x), yy = this.find(y);
-
-			this.root.put(xx, yy);
-			this.dist.put(xx, this.dist.get(y) * values[i] / this.dist.get(x));
-		}
-
-		for (int i = 0; i < M; i++) {
-            String x = queries.get(i).get(0), y = queries.get(i).get(1);
-
-			if (!this.root.containsKey(x) || !this.root.containsKey(y) || !this.find(x).equals(this.find(y))) {
-				ans[i] = -1.0;
-			} else {
-				ans[i] = this.dist.get(x) / this.dist.get(y);
-			}
-
-		}
-
-		return ans;
-	}
+            double productWeight = getPathWeight(nbrKey, end, visited, graph);
+            
+            if(productWeight != -1.0) return graph.get(start).get(nbrKey) * productWeight;
+        }
+        
+        return -1.0;
+    }
+    
+    private Map<String, Map<String, Double>> buildGraph(List<List<String>> equations, double[] values) {
+        Map<String, Map<String, Double>> g = new HashMap<>();
+        
+        for(int i=0; i<equations.size(); i++) {
+            String u = equations.get(i).get(0), v = equations.get(i).get(1);
+            
+            g.computeIfAbsent(u, k -> new HashMap<>()).put(v, values[i]);
+            g.computeIfAbsent(v, k -> new HashMap<>()).put(u, 1/values[i]);
+        }
+        
+        return g;
+    }
 }
