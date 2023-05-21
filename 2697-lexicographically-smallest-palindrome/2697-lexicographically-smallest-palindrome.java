a0c1b9563fa4a5904e5aa09b88f450b3be67aab0
Aman Kumar <kumaraman7056@hotmail.com>
2023-05-21 14:48:24 +0530

    Time: 8 ms (100.00%), Space: 44.5 MB (16.67%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
@@ -1,13 +1,9 @@
 class Solution {
     public String makeSmallestPalindrome(String s) {
-        int N = s.length();
-        int x = 0, y = N - 1;
         char[] chars = s.toCharArray();
-
-        while (x < y) {
+        
+        for(int x=0, y=s.length() - 1; x<y; x++, y--) {
             chars[y] = chars[x] = (char)Math.min((int)chars[y], (int)chars[x]);
-            x++;
-            y--;
         }
 
         return new String(chars);
