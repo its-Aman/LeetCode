0735a04191ae98cdde6e612d61aa90e20c848938
Aman Kumar <kumaraman7056@hotmail.com>
2023-05-27 06:08:50 +0530

    Time: 5 ms (99.50%), Space: 55.4 MB (45.73%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,19 +1,20 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int N = stoneValue.length;
-        int[] dp = {0,0,0};
-        int total = 0;
+        int first=0, second=0, third=0, total=0;
 
         for(int i=N-1; i>=0; i--) {
             total += stoneValue[i];
-            int curr = Math.max(total-dp[0], Math.max(total-dp[1], total-dp[2]));
-            dp[2]=dp[1];
-            dp[1]=dp[0];
-            dp[0]=curr;
+            int curr = Math.max(total-first, Math.max(total-second, total-third));
+            third=second;
+            second=first;
+            first=curr;
         }
         
-        if(dp[0] > total-dp[0]) return "Alice";
-        if(dp[0] < total-dp[0]) return "Bob";
+        total-=first;
+        
+        if(first > total) return "Alice";
+        if(first < total) return "Bob";
         
         return "Tie";
     }
