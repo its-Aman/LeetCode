af72b57767df4f99739557303dca420f8686dc4b
Aman Kumar <kumaraman7056@hotmail.com>
2023-06-03 06:58:35 +0530

    Time: 80 ms (57.33%), Space: 60.8 MB (53.86%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        List<List<Integer>> g = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            g.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (manager[i] != -1) g.get(manager[i]).add(i);
+        }
+
+        int ans = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{headID, informTime[headID]});
+
+        while (!q.isEmpty()) {
+            var curr = q.poll();
+            ans = Math.max(ans, curr[1]);
+            for (var next : g.get(curr[0])) {
+                q.offer(new int[]{next, curr[1] + informTime[next]});
+            }
+        }
+
+        return ans;
+    }
+
+    public int numOfMinutes1(int n, int headID, int[] manager, int[] informTime) {
+        if (n == 0) return 0;
+        int[] maxTimes = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            maxTimes[manager[i] + 1] = Math.max(maxTimes[manager[i] + 1], informTime[i]);
+        }
+
+        System.out.println(Arrays.toString(maxTimes));
+        return Arrays.stream(maxTimes).sum();
+    }
+    
+    public int numOfMinutes2(int n, int headID, int[] manager, int[] informTime) {
+        if (n == 0) return 0;
+        Map<Integer, Integer> g = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!g.containsKey(manager[i])) {
+                g.put(manager[i], informTime[i]);
+            } else {
+                g.put(manager[i], Math.max(informTime[i], g.get(manager[i])));
+            }
+        }
+
+        return g.values().stream().reduce((a, b) -> a + b).orElse(-1);
+    }
+}
