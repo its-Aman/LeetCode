de000a600219bd7b0d6bad024faf67b5d004ebcf
Aman Kumar <kumaraman7056@hotmail.com>
2023-06-24 07:04:53 +0530

    Time: 13 ms (96.72%), Space: 44.1 MB (60.66%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.java
+++ 0956-tallest-billboard/0956-tallest-billboard.java
@@ -1,5 +1,32 @@
 class Solution {
     public int tallestBillboard(int[] rods) {
+        int N = rods.length, sum = 0;
+        
+        for(int rod: rods) sum += rod;
+
+        int[][] dp = new int[N+1][sum+1];
+        
+        for(int i=1; i<=N; i++) {
+            for(int j=0; j<sum; j++) {
+                if(dp[i-1][j] < j) continue;
+                
+                dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
+                
+                int k=j+rods[i-1];
+                
+                dp[i][k] = Math.max(dp[i][k], dp[i-1][j] + rods[i-1]);
+                
+                k=Math.abs(j-rods[i-1]);
+                
+                dp[i][k] = Math.max(dp[i][k], dp[i-1][j] + rods[i-1]);
+
+            }
+        }
+        
+        return dp[N][0]/2;
+    }
+    
+    public int tallestBillboard2(int[] rods) {
         Map<Integer, Integer> dp = new HashMap<>();
         dp.put(0,0);
         
