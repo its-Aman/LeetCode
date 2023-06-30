24c0992d197af4e929b03c9d0e2def703e7ae9fa
Aman Kumar <kumaraman7056@hotmail.com>
2023-06-30 07:46:49 +0530

    Time: 26 ms (67.95%), Space: 55.2 MB (48.72%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution2 {
     int[][] dirs = new int[][] {{1,0}, {-1,0}, {0,1}, {0,-1}};
     int LAND=0, WATER=1, NONE=-1;
 
@@ -42,5 +42,64 @@ class Solution {
         }
         
         return false;
+    }
+    
+}
+
+class UF {
+    int[] par, size;
+    
+    public UF(int n) {
+        par = new int[n];
+        size = new int[n];
+        
+        while(n-->0) {
+            par[n]=n;
+            size[n]=1;
+        }
+    }
+    
+    public int find(int x) {
+        return (x != par[x]) ? par[x] = find(par[x]) : par[x];
+    }
+    
+    public void union(int x, int y) {
+        int xx = find(x), yy = find(y);
+        if(xx == yy) return;
+        
+        if(size[xx] < size[yy]) {
+            par[xx] = yy;            
+        } else {
+            par[yy] = xx;
+            size[yy] += size[xx];
+        }
+    }
+}
+
+class Solution {
+    int[][] dirs = new int[][] {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+    public int latestDayToCross(int R, int C, int[][] cells) {
+        UF uf = new UF(R*C+2);
+        int[][] grid = new int[R][C];
+        
+        for(int i=cells.length-1; i>=0; i--) {
+            int r = cells[i][0]-1, c = cells[i][1]-1;
+            grid[r][c] = 1;
+            int x = r*C+c+1;
+            for(var dir: dirs) {
+                int rr = r+dir[0], cc = c+dir[1];
+                int y = rr*C+cc+1;
+                
+                if(rr>=0 && rr<R && cc>=0 && cc<C && grid[rr][cc] == 1) uf.union(x,y);
+            }
+            
+            if(r==0) uf.union(0,x);
+            if(r==R-1) uf.union(R*C+1,x);
+            
+            if(uf.find(0) == uf.find(R*C+1)) return i;
+        }
+        
+        return -1;
     }
 }
