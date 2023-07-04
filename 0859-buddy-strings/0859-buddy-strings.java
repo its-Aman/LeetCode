4af72a83f579493bb3072f0a83e6a310fc142f48
Aman Kumar <kumaraman7056@hotmail.com>
2023-07-04 06:27:38 +0530

    Time: 4 ms (50.49%), Space: 41.8 MB (42.76%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+
+        if (s.equals(goal)) {
+            Set<Character> set = new HashSet<>();
+            for (char c : s.toCharArray()) set.add(c);
+            return set.size() < goal.length();
+        }
+
+        List<Integer> diff = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != goal.charAt(i)) diff.add(i);
+        }
+
+        return diff.size() == 2 && s.charAt(diff.get(0)) == goal.charAt(diff.get(1)) && s.charAt(diff.get(1)) == goal.charAt(diff.get(0));
+    }
+}
